d05fd1b05e058aa0bd85e63d8744de3a0740ee85
Stephen Jones <sjones@mpp.mpg.de>
2015-10-29 16:48:51 +0100

    Implement U,F routines + unittest

---
+++ pySecDec/uf.py
@@ -0,0 +1,36 @@
+"""The U, F routes"""
+
+import sympy as sp
+    
+def uf(str_ls,str_props):
+    r'''
+    Construct the 1st (U) and 2nd (F) Symanzik Polynomials. Outputs a tuple containing (U,F).
+    
+    :param str_ls:
+       list of strings;
+       The loop momenta
+       
+    :param str_props:
+       list of strings:
+       The propagators
+    
+    Adapted from A.V. Smirnov's Mathematica UF routine: http://science.sander.su/Tools-UF.htm
+    '''
+    ls = sp.sympify(str_ls)
+    props = sp.sympify(str_props)
+
+    # Feynman parameters
+    x = sp.symbols('x')
+    
+    u = 1
+    f = -sum(prop*x(i) for i,prop in enumerate(props))
+    
+    for l in ls:
+        t0, t1, t2 = reversed(sp.Poly(f,l).all_coeffs())
+        u *= t2
+        f = sp.together(t0-(t1**2)/(4*t2))
+
+    f = sp.ratsimp(-u*f) # todo: need minus sign?
+    u = sp.ratsimp(u)
+
+    return (u,f)
