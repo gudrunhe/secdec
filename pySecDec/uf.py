6d88052c7b0d70ed648ca5d3180da2833b4d0cd7
Stephen Jones <sjones@mpp.mpg.de>
2015-10-29 18:49:02 +0100

    change syntax for feynman parameters

--- pySecDec/uf.py
+++ pySecDec/uf.py
@@ -22,14 +22,15 @@ def uf(str_ls,str_props):
 
     # Feynman parameters
     x = sp.symbols('x')
+    x = [sp.symbols('x%i' % i) for i,_ in enumerate(props)]
     
     u = 1
-    f = -sum(prop*x(i) for i,prop in enumerate(props))
+    f = -sum(prop*x[i] for i,prop in enumerate(props))
 
     for l in ls:
         t0, t1, t2 = reversed(sp.Poly(f,l).all_coeffs())
         u *= t2
-        f = sp.together(t0-(t1**2)/(4*t2))
+        f = sp.together((t0*t2-t1**2/4)/t2)
     
     f = sp.ratsimp(-u*f) # todo: need minus sign?
     u = sp.ratsimp(u)
