6284e0a61a58badf2af75ca2a7ed20f9defa1f8a
Stephan Jahn <sjahn@mpp.mpg.de>
2015-11-27 20:24:53 +0100

    implement class `Polytope`
    `Polytope` interfaces the command line tool `normaliz`

--- pySecDec/decomposition.py
+++ pySecDec/decomposition.py
@@ -1,11 +1,14 @@
 """The sector decomposition routines"""
 
+from __future__ import print_function
 from .polynomial import Polynomial, PolynomialProduct
 from .sector import Sector
-from.misc import powerset
-import numpy as np
+from .misc import powerset
+import subprocess, shutil, os, re, numpy as np
 
-# ********************** primary decomposition **********************
+# TODO: Split this file into two files - one for the iterative method and one for the geomethod.
+
+# ********************** iterative decomposition **********************
 
 def primary_decomposition_polynomial(polynomial):
     r'''
@@ -76,8 +79,6 @@ def primary_decomposition(sector):
         ) for sector_index in range(N)
     ]
     return primary_sectors
-
-# ********************** iterative decomposition **********************
 
 class EndOfDecomposition(Exception):
     '''
@@ -271,3 +272,166 @@ def convex_hull(*polynomials):
         product *= poly
 
     return product.expolist[np.where( product.coeffs == 1 )]
+
+class Polytope(object):
+    r'''
+    Representation of a polytope defined by either its
+    vertices or its facets. Call
+    :meth:`.complete_representation` to translate from
+    one to the other representation.
+
+    :param vertices:
+        two dimensional array;
+        The polytope in vertex representation. Each
+        row is interpreted as one vertex.
+
+    :param facets:
+        two dimensional array;
+        The polytope in facet representation. Each
+        row represents one facet :math:`F`.
+        A row in `facets` is interpreted as one normal
+        vector :math:`n_F` with additionally the constant
+        :math:`a_F` in the last column.
+        The points :math:`v` of the polytope obey
+
+        .. math::
+            \bigcap_F \left( {\langle n_F, v \rangle} + a_F \right) \ge 0
+
+    '''
+    def __init__(self, vertices=None, facets=None):
+        if vertices is None and facets is None or vertices is not None and facets is not None:
+            raise TypeError("Pass either `vertices` or `facets` but not both.")
+
+        # copy and convert to numpy array
+        if vertices is None:
+            self.vertices = None
+        else:
+            self.vertices = np.array(vertices)
+
+        if facets is None:
+            self.facets = None
+        else:
+            self.facets = np.array(facets)
+
+    def complete_representation(self, normaliz='normaliz', workdir='normaliz_tmp', keep_workdir=False, verbose=False):
+        '''
+        Transform the vertex representation of a polytope
+        to the facet representation or the other way round.
+        Remove surplus entries in ``self.facets`` or
+        ``self.vertices``.
+
+        .. note::
+            This function calls the command line executable of
+            `normaliz <http://www.home.uni-osnabrueck.de/wbruns/
+            normaliz/>`_..
+
+        :param normaliz:
+            string or None;
+            If this is None, return the `normaliz` run card
+            as string.
+            Otherwise, this specifies the shell command to
+            run `normaliz`.
+
+        :param workdir:
+            string;
+            The directory for the communication with `normaliz`.
+            A directory with the specified name will be created
+            in the current working directory. If the specified
+            directory name already exists, an :class:`OSError`
+            is raised.
+
+            .. note::
+                The communication with `normaliz` is done via
+                files.
+
+        :param keep_workdir:
+            bool;
+            Whether or not to delete the `workdir` after execution.
+
+        :param verbose:
+            bool;
+            Whether or not to print the command line messages
+            of `normaliz`.
+
+        '''
+        os.mkdir(workdir)
+        try:
+            if self.facets is None:
+                run_card_as_str = self._make_run_card_vertices2facets()
+            elif self.vertices is None:
+                run_card_as_str = self._make_run_card_facets2vertices()
+            else:
+                raise ValueError('Both representations (facet and vertex) are already calculated')
+
+            run_card_file_prefix = 'normaliz'
+            run_card_file_suffix = '.in'
+            run_card_filename = run_card_file_prefix + run_card_file_suffix
+            normaliz_args = ['--ext', '--cst'] # create the files 'normaliz.ext' (vertices) and 'normaliz.cst' (facets)
+            command_line_command = [normaliz] + normaliz_args + [run_card_filename]
+
+            # dump run card to file
+            with open(os.path.join(workdir, run_card_filename),'w') as f:
+                f.write(run_card_as_str)
+
+            if verbose:
+                print('Normaliz run card (file "%s"):\n' % run_card_filename)
+                print('-----------------------------------')
+                print(run_card_as_str)
+                print('-----------------------------------')
+                print()
+                print('running "%s" ...\n' % ' '.join(command_line_command))
+
+            # run normaliz
+            #    stdout=None and stderr=None --> print all normaliz output to the screen
+            #    stdout=subprocess.PIPE and stderr=subprocess.PIPE --> redirect and discard output
+            #    subprocess.check_call --> run normaliz, block until it finishes and raise error on nonzero exit status
+            subprocess.check_call(command_line_command, stdout=None if verbose else subprocess.PIPE, stderr=None if verbose else subprocess.PIPE, cwd=workdir)
+
+            # read file output from normaliz
+            if self.facets is None: # reduced vertices are in cst file
+                vertices = self._read_ext_file(os.path.join(workdir,run_card_file_prefix + '.ext'))
+                self.facets = self._read_cst_file(os.path.join(workdir,run_card_file_prefix + '.cst'))
+            elif self.vertices is None: # reduced facets are in cst file
+                self.facets = self._read_ext_file(os.path.join(workdir,run_card_file_prefix + '.ext'))
+                vertices = self._read_cst_file(os.path.join(workdir,run_card_file_prefix + '.cst'))
+            else:
+                raise RuntimeError()
+
+            # discard the last column that only consists of ones
+            self.vertices = vertices[:,:-1]
+
+        finally:
+            if not keep_workdir:
+                shutil.rmtree(workdir)
+
+    def _make_run_card_vertices2facets(self):
+        run_card_as_str  = str(self.vertices.shape[0]) + ' ' + str(self.vertices.shape[1]) + '\n\n'
+        run_card_as_str += str(self.vertices).replace('[',' ').replace(']',' ')
+        run_card_as_str += '\n\npolytope\n'
+        return run_card_as_str
+
+    def _make_run_card_facets2vertices(self):
+        run_card_as_str  = str(self.facets.shape[0]) + ' ' + str(self.facets.shape[1]) + '\n'
+        run_card_as_str += str(self.facets).replace('[','').replace(']','').replace('\n ','\n')
+        run_card_as_str += 'integral_closure\n'
+        return run_card_as_str
+
+    def _read_cst_file(self, filepath):
+        with open(filepath, 'r') as f:
+            # the first two lines contain the array dimensions
+            shape = int(f.readline()), int(f.readline())
+
+            # the reduced input comes next (space and newline separated) and is terminated by a line containing letters
+            array_as_str = ''
+            current_str = f.readline()
+            while re.match(r'^[\-0-9 ]+$', current_str) is not None:
+                array_as_str += current_str
+                current_str = f.readline()
+
+        return np.fromstring(array_as_str, sep=' ', dtype=int).reshape(shape)
+
+    def _read_ext_file(self, filepath):
+        with open(filepath, 'r') as f:
+            # the first two lines contain the array dimensions
+            shape = int(f.readline()), int(f.readline())
+            return np.fromfile(f, sep=' ', dtype=int).reshape(shape)
--- pySecDec/test_decomposition.py
+++ pySecDec/test_decomposition.py
@@ -3,8 +3,16 @@
 from .decomposition import *
 from .polynomial import Polynomial, PolynomialProduct
 from .sector import Sector
+from .misc import argsort_2D_array
 import numpy as np
+import sys
 import unittest
+
+python_major_version = sys.version[0]
+
+def sort_2D_array(array):
+    'Use the .misc.argsort_2D_array function to sort an array'
+    return array[argsort_2D_array(array)]
 
 class TestPrimaryDecomposition(unittest.TestCase):
     def setUp(self):
@@ -171,13 +179,83 @@ class TestGeomethod(unittest.TestCase):
         p0 = Polynomial.from_expression('x0+x1+x0*x1', ['x0','x1'])
         p1 = Polynomial.from_expression('1+x0+x1', ['x0','x1'])
 
-        # TODO use function `sort_2D_array`
         hull = convex_hull(p0,p1)
-        target_hull = [[2,1],
-                       [1,2],
-                       [2,0],
-                       [1,0],
-                       [0,2],
-                       [0,1]]
+        target_hull = np.array([[2,1],
+                                [1,2],
+                                [2,0],
+                                [1,0],
+                                [0,2],
+                                [0,1]])
 
-        np.testing.assert_array_equal(hull, target_hull)
+        # The ordering is not important but must be fixed to compare the arrays
+        sorted_hull = sort_2D_array(hull)
+        sorted_target_hull = sort_2D_array(target_hull)
+        np.testing.assert_array_equal(sorted_hull, sorted_target_hull)
+
+class TestPolytope(unittest.TestCase):
+    def setUp(self):
+        self.vertices = [[2,1],
+                         [1,2],
+                         [2,0],
+                         [1,0],
+                         [0,2],
+                         [0,1]]
+
+        self.vertices_with_inside = [[1,1]] + self.vertices
+
+        self.facets = [[ 0, 1, 0],
+                       [ 1, 0, 0],
+                       [-1, 0, 2],
+                       [ 0,-1, 2],
+                       [ 1, 1,-1],
+                       [-1,-1, 3]]
+
+        self.facets_with_outside = [[ 0, 1, 5]] + self.facets
+
+    def test_init(self):
+        # must provide either facets or vertices
+        self.assertRaisesRegexp(TypeError, 'either.*vertices.*or.*facets', Polytope, facets=self.facets, vertices=self.vertices)
+        self.assertRaisesRegexp(TypeError, 'either.*vertices.*or.*facets', Polytope)
+
+        inarray = np.array([[1,2,3],
+                            [4,5,6]])
+
+        polytope1 = Polytope(facets=inarray)
+        polytope2 = Polytope(vertices=inarray)
+
+        inarray[0,0] = 10
+
+        self.assertEqual(polytope1.facets[0,0], 1)
+        self.assertEqual(polytope2.vertices[0,0], 1)
+
+    def test_vertex2facet(self):
+        polytope1 = Polytope(vertices=self.vertices)
+        polytope2 = Polytope(vertices=self.vertices_with_inside)
+
+        polytope1.complete_representation(workdir='tmpdir1_test_vertex2facet_python' + python_major_version)
+        polytope2.complete_representation(workdir='tmpdir2_test_vertex2facet_python' + python_major_version)
+
+        self.assertRaisesRegexp(ValueError, '(B|b)oth.*already', polytope1.complete_representation, workdir='tmpdir3_test_vertex2facet_python' + python_major_version)
+        self.assertRaisesRegexp(ValueError, '(B|b)oth.*already', polytope2.complete_representation, workdir='tmpdir4_test_vertex2facet_python' + python_major_version)
+
+        # The ordering is not important but must be fixed to compare the arrays
+        np.testing.assert_array_equal( sort_2D_array(np.array(polytope1.vertices)), sort_2D_array(np.array(self.vertices)) )
+        np.testing.assert_array_equal( sort_2D_array(np.array(polytope2.vertices)), sort_2D_array(np.array(self.vertices)) )
+        np.testing.assert_array_equal( sort_2D_array(np.array(polytope1.facets)), sort_2D_array(np.array(self.facets)) )
+        np.testing.assert_array_equal( sort_2D_array(np.array(polytope2.facets)), sort_2D_array(np.array(self.facets)) )
+
+    def test_facet2vertex(self):
+        polytope1 = Polytope(facets=self.facets)
+        polytope2 = Polytope(facets=self.facets_with_outside)
+
+        polytope1.complete_representation(workdir='tmpdir1_test_facet2vertex_python' + python_major_version)
+        polytope2.complete_representation(workdir='tmpdir2_test_facet2vertex_python' + python_major_version)
+
+        self.assertRaisesRegexp(ValueError, '(B|b)oth.*already', polytope1.complete_representation, workdir='tmpdir3_test_facet2vertex_python' + python_major_version)
+        self.assertRaisesRegexp(ValueError, '(B|b)oth.*already', polytope2.complete_representation, workdir='tmpdir4_test_facet2vertex_python' + python_major_version)
+
+        # The ordering is not important but must be fixed to compare the arrays
+        np.testing.assert_array_equal( sort_2D_array(np.array(polytope1.vertices)), sort_2D_array(np.array(self.vertices)) )
+        np.testing.assert_array_equal( sort_2D_array(np.array(polytope2.vertices)), sort_2D_array(np.array(self.vertices)) )
+        np.testing.assert_array_equal( sort_2D_array(np.array(polytope1.facets)), sort_2D_array(np.array(self.facets)) )
+        np.testing.assert_array_equal( sort_2D_array(np.array(polytope2.facets)), sort_2D_array(np.array(self.facets)) )
