dc80d7aef22d88eba868a5cef1e77bb4f3a7432d
Stephan Jahn <sjahn@mpp.mpg.de>
2016-04-11 11:49:46 +0200

    increase version number for minor release

--- pySecDec/metadata.py
+++ pySecDec/metadata.py
@@ -1,2 +1,2 @@
-version = __version__ = '0.1'
+version = __version__ = '0.1.1'
 authors = __authors__ = 'Sophia Borowka, Gudrun Heinrich, Stephan Jahn, Stephen Jones, Matthias Kerner, Johannes Schlenk, Tom Zirke'
