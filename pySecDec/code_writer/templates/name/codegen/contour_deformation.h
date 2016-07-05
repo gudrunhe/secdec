d2fc5ea414fd0e67c359ad9e1939f151cf59bf7d
Stephan Jahn <sjahn@mpp.mpg.de>
2016-07-05 17:32:39 +0200

    [contour_deformation] obvious adaptations to new syntax

--- pySecDec/code_writer/templates/name/codegen/contour_deformation.h
+++ pySecDec/code_writer/templates/name/codegen/contour_deformation.h
@@ -7,6 +7,10 @@ Symbols labelTransform, labelJacobiI, labelJacobiJ;
 #define deformationParameters "%(deformation_parameters)s"
 Symbols `deformationParameters';
 
+* Define the name of the polynomial for the contour deformation
+* ("F" in loop integrals)
+#define F "%(contour_deformation_polynomial)s"
+
 * The transformation of the Feynman parameters
 * (z_k({x_k}) = x_k - i * lambda_k * (1 - x_k) * Re(dF_dx_k))
 * and its Jacobian matrix suitable for simultaneous
