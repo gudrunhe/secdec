a5753f7ffdd5e6d3f24a565bceb2f533c354f764
Stephan Jahn <sjahn@mpp.mpg.de>
2016-07-05 17:29:26 +0200

    [make_package/test.cpp] adapt to new syntax

--- pySecDec/code_writer/templates/name/test.cpp
+++ pySecDec/code_writer/templates/name/test.cpp
@@ -25,8 +25,8 @@
 
 struct userdata_t {
     const int epsilon_order;
-    %(name)s::Mandelstam_t const * const Mandelstam;
-    %(name)s::mass_t const * const mass;
+    %(name)s::real_t const * const real_parameters;
+    %(name)s::complex_t const * const complex_parameters;
 };
 
 static int cuba_integrand(const int *ndim, const cubareal xx[], const int *ncomp, cubareal ff[], void *userdata)
@@ -35,30 +35,30 @@ static int cuba_integrand(const int *ndim, const cubareal xx[], const int *ncomp
     ff[0] = 0.;
     for (const auto &sector : %(name)s::sectors)
     {
-        if (data.epsilon_order >= sector.order_min && data.epsilon_order <= sector.order_max)
-            ff[0] += sector[data.epsilon_order].integrand(xx, data.Mandelstam, data.mass);
+        if (data.epsilon_order >= sector.get_order_min() && data.epsilon_order <= sector.get_order_max())
+            ff[0] += sector[data.epsilon_order].integrand(xx, data.real_parameters, data.complex_parameters);
         // else: does not contribute
     }
     return 0;
 }
 
-void integrate(const std::vector<%(name)s::Mandelstam_t> &Mandelstam, const std::vector<%(name)s::mass_t> &mass)
+void integrate(const std::vector<%(name)s::real_t> &real_parameters, const std::vector<%(name)s::complex_t> &complex_parameters)
 {
 
     std::cout << "-- Integrating --" << std::endl;
 
-    if ( Mandelstam.size() != %(name)s::number_of_Mandelstam_variables )
-        throw std::logic_error("Did not set the correct number of Mandelstam variables");
+    if ( real_parameters.size() != %(name)s::number_of_real_parameters )
+        throw std::logic_error("Did not set the correct number of real parameters");
 
-    if ( mass.size() != + %(name)s::number_of_masses )
-        throw std::logic_error("Did not set the correct number of masses");
+    if ( complex_parameters.size() != + %(name)s::number_of_complex_parameters )
+        throw std::logic_error("Did not set the correct number of complex parameters");
 
-    int lowest_epsilon_order = %(name)s::sectors.at(0).order_min;
-    int highest_epsilon_order = %(name)s::sectors.at(0).order_max;
+    int lowest_epsilon_order = %(name)s::sectors.at(0).get_order_min();
+    int highest_epsilon_order = %(name)s::sectors.at(0).get_order_max();
     for (const auto &sector : %(name)s::sectors)
     {
-        lowest_epsilon_order = std::min(lowest_epsilon_order, sector.order_min);
-        highest_epsilon_order = std::max(highest_epsilon_order, sector.order_max);
+        lowest_epsilon_order = std::min(lowest_epsilon_order, sector.get_order_min());
+        highest_epsilon_order = std::max(highest_epsilon_order, sector.get_order_max());
     }
 
     for ( int epsilon_order = lowest_epsilon_order; epsilon_order <= highest_epsilon_order; ++epsilon_order )
@@ -71,12 +71,12 @@ void integrate(const std::vector<%(name)s::Mandelstam_t> &Mandelstam, const std:
         int ndim = 0;
         for (const auto &sector : %(name)s::sectors)
         {
-            if (epsilon_order >= sector.order_min && epsilon_order <= sector.order_max)
-                ndim = std::max(ndim, sector[epsilon_order].number_of_Feynman_parameters);
+            if (epsilon_order >= sector.get_order_min() && epsilon_order <= sector.get_order_max())
+                ndim = std::max(ndim, sector[epsilon_order].number_of_integration_variables);
             // else: does not contribute
         }
 
-        userdata_t userdata{epsilon_order, Mandelstam.data(), mass.data()};
+        userdata_t userdata{epsilon_order, real_parameters.data(), complex_parameters.data()};
 
         Vegas(ndim, NCOMP, cuba_integrand, reinterpret_cast<void *>(&userdata), NVEC,
               EPSREL, EPSABS, VERBOSE, SEED,
@@ -98,10 +98,10 @@ void integrate(const std::vector<%(name)s::Mandelstam_t> &Mandelstam, const std:
 int main()
 {
     // User Specified Phase-space point
-    const std::vector<%(name)s::Mandelstam_t> Mandelstam = { -2., -1.5 };
-    const std::vector<%(name)s::mass_t> mass = { 1. };
+    const std::vector<%(name)s::real_t> real_parameters = { -3., -2. };
+    const std::vector<%(name)s::complex_t> complex_parameters = {  };
 
-    integrate(Mandelstam, mass);
+    integrate(real_parameters, complex_parameters);
 
     return 0;
 }
