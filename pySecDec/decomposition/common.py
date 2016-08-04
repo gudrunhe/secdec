608fb3817546c4f66d2981888d6f38e7bbc7956e
Stephan Jahn <sjahn@mpp.mpg.de>
2016-08-04 21:25:56 +0200

    [symmetries] implement function to find symmetries between sectors

--- pySecDec/decomposition/common.py
+++ pySecDec/decomposition/common.py
@@ -1,7 +1,9 @@
 """The Sector class"""
 
 from ..algebra import Polynomial, ExponentiatedPolynomial, Product
+from ..misc import argsort_2D_array, argsort_ND_array
 import numpy as np
+import sympy as sp
 
 class Sector(object):
     '''
@@ -122,6 +124,184 @@ def refactorize(polyprod, parameter=None):
         expolist_mono[:,parameter] += factorizable_power
         expolist_poly[:,parameter] -= factorizable_power
 
+# -------------------- finding symmetries ---------------------
+
+def _sector2array(sector):
+    '''
+    Combine the `expolist`s and the `coeff`s
+    of all :class:`.Polynomial`s in a
+    :class:`.Sector` to two large arrays.
+    Return the combined expolists and the
+    combined coeffs.
+
+    :param sector:
+        :class:`.Sector`; The container of the
+        :class:`.Polynomial`s to be combined.
+
+    '''
+    # process `Jacobian`
+    combined_expolists = [sector.Jacobian.expolist]
+    combined_coeffs = [1] # Jacobian coefficient can be ignored when searching for symmetries
+
+    # process `cast`
+    for index,prod in enumerate(sector.cast):
+        # symmetries may be hidden by the factorization --> undo it
+        factors_as_polynomials = [Polynomial(factor.expolist, factor.coeffs, factor.polysymbols, copy=False) for factor in prod.factors]
+        multiplied_prod = factors_as_polynomials[0] * factors_as_polynomials[1]
+        combined_expolists.extend(multiplied_prod.expolist)
+        # must distinguish between the individual polynomials and between `Polynomial` and `ExponentiatedPolynomial`
+        if type(prod.factors[0]) is ExponentiatedPolynomial:
+            combined_coeffs.extend(multiplied_prod.coeffs * sp.sympify('SecDecInternalExponent(%s)*SecDecInternalCast(%i)'%(prod.factors[0].exponent,index)))
+        else:
+            combined_coeffs.extend(multiplied_prod.coeffs * sp.sympify('SecDecInternalCast(%i)'%index))
+
+    # process `other`
+    for index,poly in enumerate(sector.other):
+        combined_expolists.append(poly.expolist)
+        # must distinguish between `Polynomial` and `ExponentiatedPolynomial`
+        if type(prod.factors[0]) is ExponentiatedPolynomial:
+            combined_coeffs.extend(poly.coeffs * sp.sympify('SecDecInternalExponent(%s)*SecDecInternalOther(%i)'%(prod.factors[0].exponent,index)))
+        else:
+            combined_coeffs.extend(poly.coeffs * sp.sympify('SecDecInternalOther(%i)'%index))
+
+    # return as type `numpy.ndarray`
+    return np.vstack(combined_expolists), np.hstack(combined_coeffs)
+
+def _collision_safe_hash(iterable):
+    '''
+    Return an array containingh the hashes of
+    an `iterable`. If there is a hash collision
+    (if the hashes of two objects that do not
+    compare equal are equal), alter the hashes
+    until there is no collision any more.
+
+    :param iterable:
+        iterable of hashable objects;
+        The objects to be hashed.
+
+    '''
+    objects = np.asarray(iterable)
+    hashes = np.array([hash(i) for i in objects])
+
+    number_of_differing_objects = len( set(objects) )
+    number_of_differing_hashes = len( set(hashes) )
+
+    while number_of_differing_objects != number_of_differing_hashes: # while there is a hash collision
+        # find the hash with collision
+        for i,hash_value in enumerate(hashes):
+            # There is a hash collision with ``objects[i]`` if its hash occurs
+            # later in the list of `hashes`, but the ``objects[i]`` itself does
+            # not appear any more.
+            if hash_value in hashes[i+1:] and objects[i] not in objects[i+1:]:
+                break
+
+        # add one to the hashes of one of the corresponding object
+        hashes[np.where(objects == objects[i])] += 1
+
+        # update `number_of_differing_hashes`
+        number_of_differing_hashes = len( set(hashes) )
+
+    return np.array(hashes)
+
+def drop_symmetry_redundant_sectors(sectors):
+    '''
+    Reduce a list of sectors by squashing duplicates
+    with equal integral.
+    If two sectors only differ by a permutation of the
+    polysymbols (to be interpreted as integration
+    variables over some inteval), then the two sectors
+    integrate to the same value. Thus we can drop one
+    of them and count the other twice. The multiple
+    counting of a sector is accounted for by increasing
+    the coefficient of the Jacobian by one.
+
+    Example:
+
+    >>> from pySecDec.algebra import Polynomial
+    >>> from pySecDec.decomposition import Sector
+    >>> from pySecDec.decomposition import drop_symmetry_redundant_sectors
+    >>>
+    >>> poly = Polynomial([(0,1),(1,0)], ['a','b'])
+    >>> swap = Polynomial([(1,0),(0,1)], ['a','b'])
+    >>> Jacobian_poly = Polynomial([(1,0)], [3]) # three
+    >>> Jacobian_swap = Polynomial([(0,1)], [5]) # five
+    >>> sectors = (
+    ...               Sector([poly],Jacobian=Jacobian_poly),
+    ...               Sector([swap],Jacobian=Jacobian_swap)
+    ...           )
+    >>>
+    >>> reduced_sectors = drop_symmetry_redundant_sectors(sectors)
+    >>> len(reduced_sectors) # symmetry x0 <--> x1
+    1
+    >>> # The Jacobians are added together to account
+    >>> # for the double counting of the sector.
+    >>> reduced_sectors[0].Jacobian
+     + (8)*x0
+
+    :param sectors:
+        iterable of :class:`.Sector`; the sectors to be
+        reduced.
+
+    '''
+    def sort(array):
+        # keep sorting the 2D array (in place) along both dimensions
+        # until it no longer changes
+
+        # initilize sort keys such that we enter the while loop at least once
+        sort_key_axis_0 = None
+        sort_key_axis_1 = None
+
+        while not ( np.array_equal(sort_key_axis_0, np.arange(array.shape[0])) and np.array_equal(sort_key_axis_1, np.arange(array.shape[1]-1)) ):
+            # sort along axis 0
+            sort_key_axis_0 = argsort_2D_array(array)
+            array[:] = array[sort_key_axis_0]
+
+            # sort along axis 1 excluding the coefficients (first column)
+            sort_key_axis_1 = argsort_2D_array(array.T[1:])
+            array[:,1:] = array[:,1:][:,sort_key_axis_1]
+
+    if not isinstance(sectors, list):
+        sectors = list(sectors)
+
+    # combine all expolists and coeffs into one large array
+    # use the collision free hash for the coefficients
+    all_sectors_array = []
+    for sector in sectors:
+        this_sector_expolist, this_sector_coeffs = _sector2array(sector)
+        this_sector_coeffs = _collision_safe_hash(this_sector_coeffs)
+
+        this_sector_array = np.hstack([this_sector_coeffs.reshape(len(this_sector_coeffs),1),this_sector_expolist])
+
+        # sort the arrays of the individual sectors to pick one specific permutation --> symmetry finding
+        sort(this_sector_array)
+
+        all_sectors_array.append( this_sector_array )
+
+    all_sectors_array = np.array(all_sectors_array)
+
+    # sort the sectors
+    indices_sorted_sectors = argsort_ND_array(all_sectors_array)
+
+    # prune sectors that are equal
+    # Since we sorted the sectors above, we only need to consider the
+    # sectors that next to each other.
+    previous_index = indices_sorted_sectors[0]
+    previous_sector_array = all_sectors_array[previous_index]
+    previous_sector = sectors[previous_index].copy()
+    output = [previous_sector]
+    for sector_index in indices_sorted_sectors[1:]:
+        if np.array_equal(all_sectors_array[sector_index],previous_sector_array):
+            # squash this sector into the previous one
+            previous_sector.Jacobian.coeffs[0] += sectors[sector_index].Jacobian.coeffs[0]
+        else:
+            # this sector is not equal to the previous one --> update output
+            previous_index = indices_sorted_sectors[sector_index]
+            previous_sector_array = all_sectors_array[sector_index]
+            previous_sector = sectors[sector_index].copy()
+            output.append(previous_sector)
+
+    return output
+
 # -------------------- `hide` and `unhide` --------------------
 
 def hide(polynomial, count):
@@ -170,5 +350,3 @@ def unhide(polynomial1, polynomial2):
     polynomial1.expolist = np.hstack([polynomial1.expolist, polynomial2.expolist])
     polynomial1.coeffs = polynomial2.coeffs
     return polynomial1
-
-# -------------------------------------------------------------
