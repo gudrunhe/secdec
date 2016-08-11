60ed55613af494f9143d2d3e4d826bc2050c9352
Stephan Jahn <sjahn@mpp.mpg.de>
2016-08-11 11:53:18 +0200

    add `git_id` to metadata; hard-code it when creating a distribution

--- setup.py
+++ setup.py
@@ -6,11 +6,6 @@ use_setuptools()
 
 from setuptools import setup, find_packages
 
-# get the version number and author list from package
-import sys
-sys.path.insert(0, 'pySecDec')
-from metadata import __version__, __authors__
-
 # collect the c++ template files needed by `pySecDec.code_writer.make_package`
 import os
 cpp_template_files = []
@@ -20,27 +15,56 @@ for dir_name, subdir_list, file_list in os.walk(relpath_to_code_writer_module):
     for data_file in file_list:
         cpp_template_files.append( os.path.join(relpath_to_templates, data_file) )
 
-setup(
-    name='pySecDec',
-    packages=find_packages(),
-    package_data={'pySecDec.code_writer': cpp_template_files},
-    version=__version__,
-    author=__authors__,
-    author_email='secdec@projects.hepforge.org',
-    url='secdec.hepforge.org',
-    description='An implementation of "Sector Decomposition" (see arXiv:hep-ph/0004013, arXiv:0803.4177).', # TODO: cite paper for this SecDec version
-    license='GPLv3',
-    install_requires=['numpy>=1.6', 'sympy', 'setuptools>=3.3'],
-    extras_require={'testing': ['nose'], 'documentation': ['sphinx']},
-    classifiers=['Development Status :: 3 - Alpha',
-                 'Environment :: Console',
-                 'Intended Audience :: Science/Research',
-                 'Natural Language :: English',
-                 'Operating System :: Unix',
-                 'Programming Language :: Python :: 2.7',
-                 'Programming Language :: Python :: 3',
-                 'Programming Language :: C++',
-                 'Topic :: Scientific/Engineering :: Physics',
-                 ],
-    platforms=['Unix'],
+# get the git commit id
+from subprocess import check_output as shell_call, CalledProcessError
+try:
+    git_id = shell_call(['git','rev-parse','HEAD'], cwd=os.path.split(os.path.abspath(__file__))[0]).strip()
+except CalledProcessError:
+    git_id = 'UNKNOWN'
+# in python3, the return type of `shell_call` may be `bytes` but we need `str`
+if not isinstance(git_id, str):
+    git_id = git_id.decode()
+
+# hard-code the current git commit id into `metadata.py`
+import shutil
+shutil.copy(os.path.join('pySecDec','metadata.py'), os.path.join('pySecDec','metadata.py.orig'))
+try:
+    # hard-code the current git commit id into `metadata.py` (continued)
+    with open(os.path.join('pySecDec','metadata.py'), 'r') as metadata_file:
+        metadata = metadata_file.read()
+    metadata = metadata.replace("git_id = shell_call(['git','rev-parse','HEAD'], cwd=os.path.split(os.path.abspath(__file__))[0]).strip()", "git_id = '" + git_id + "'")
+    with open(os.path.join('pySecDec','metadata.py'), 'w') as metadata_file:
+        metadata_file.write(metadata)
+
+    # get the version number and author list from package
+    import sys
+    sys.path.insert(0, 'pySecDec')
+    from metadata import __version__, __authors__
+
+    setup(
+        name='pySecDec',
+        packages=find_packages(),
+        package_data={'pySecDec.code_writer': cpp_template_files},
+        version=__version__,
+        author=__authors__,
+        author_email='secdec@projects.hepforge.org',
+        url='secdec.hepforge.org',
+        description='An implementation of "Sector Decomposition" (see arXiv:hep-ph/0004013, arXiv:0803.4177).', # TODO: cite paper for this SecDec version
+        license='GPLv3',
+        install_requires=['numpy>=1.6', 'sympy', 'setuptools>=3.3'],
+        extras_require={'testing': ['nose'], 'documentation': ['sphinx']},
+        classifiers=['Development Status :: 3 - Alpha',
+                     'Environment :: Console',
+                     'Intended Audience :: Science/Research',
+                     'Natural Language :: English',
+                     'Operating System :: Unix',
+                     'Programming Language :: Python :: 2.7',
+                     'Programming Language :: Python :: 3',
+                     'Programming Language :: C++',
+                     'Topic :: Scientific/Engineering :: Physics',
+                     ],
+        platforms=['Unix'],
     )
+finally:
+    # restore the original `metadata.py`
+    shutil.move(os.path.join('pySecDec','metadata.py.orig'), os.path.join('pySecDec','metadata.py'))
