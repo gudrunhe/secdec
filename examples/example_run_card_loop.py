18b8d357b1f11ec4b2b9daefac4484a8ecb38ff5
Stephan Jahn <sjahn@mpp.mpg.de>
2016-08-14 13:24:31 +0200

    [examples] switch to 2L triangle

--- examples/example_run_card_loop.py
+++ examples/example_run_card_loop.py
@@ -2,35 +2,32 @@
 from pySecDec.loop_integral import loop_package
 import pySecDec as psd
 
-
-li = psd.loop_integral.LoopIntegralFromPropagators(
-propagators = ['k1**2','(k1+p2)**2','(k1-p1)**2','(k1-k2)**2','(k2+p2)**2','(k2-p1)**2','(k2+p2+p3)**2','(k1+p3)**2'],
-loop_momenta = ['k1','k2'],
-powerlist = [1,1,1,1,1,1,1,0],
-external_momenta = ['p1','p2','p3','p4'],
+li = psd.loop_integral.LoopIntegralFromGraph(
+internal_lines = [['m',[3,4]],['m',[4,5]],['m',[3,5]],[0,[1,2]],[0,[4,1]],[0,[2,5]]],
+external_lines = [['p1',1],['p2',2],['p3',3]],
 
 replacement_rules = [
                         ('p1*p1', 0),
                         ('p2*p2', 0),
-                        ('p3*p3', 0),
+                        ('p3*p3', 's'),
                         ('p4*p4', 0),
                         ('p1*p2', 's/2'),
-                        ('p2*p3', 't/2'),
-                        ('p1*p3', '-s/2-t/2')
+                        ('p2*p3', '-s/2'),
+                        ('p1*p3', '-s/2'),
+                        ('m**2', 'msq')
                     ]
-
 )
 
 
-Mandelstam_symbols = ['s', 't']
-mass_symbols = []
+Mandelstam_symbols = ['s']
+mass_symbols = ['msq']
 
 
 loop_package(
 
 target_directory = 'pySecDec_loop_integrals',
 
-name = 'box2l',
+name = 'triangle',
 
 loop_integral = li,
 
@@ -43,7 +40,7 @@ requested_order = 0,
 form_optimization_level = 2,
 
 # the WorkSpace parameter for FORM
-form_work_space = '10G',
+form_work_space = '500M',
 
 # whether or not to increase numerical stability
 # Note: This is very extensive concerning both - the algebra and the numerics.
@@ -57,6 +54,6 @@ decomposition_method = 'geometric',
 # whether or not to produce code to perform the contour deformation
 # if ``True``, it can still be deactivated in the "config.hpp"
 # if ``False``, no code for the contour deformation is generated
-contour_deformation = False,
+contour_deformation = True,
 
 )
