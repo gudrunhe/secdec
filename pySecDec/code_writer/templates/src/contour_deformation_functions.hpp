b27ee34fc1bcc344b1c1c3379b66f864f7136ee1
Stephan Jahn <sjahn@mpp.mpg.de>
2016-09-12 19:21:18 +0200

    [make_package/contour_deformation_functions] speed improvements

--- pySecDec/code_writer/templates/src/contour_deformation_functions.hpp
+++ pySecDec/code_writer/templates/src/contour_deformation_functions.hpp
@@ -7,39 +7,29 @@ namespace %(name)s
 {
     // required functions for contour deformation
 
-    /*
-     * Numerically stable implementation of x*exp(-mu/x)
-     * for x in [0,1].
-     */
     inline real_t SecDecInternalXExpMinusMuOverX(real_t mu, real_t x)
     {
         if (x == 0)
             return 0;
 
-        // x*exp(-mu/x) = exp(-mu/x + log(x))
-        return std::exp(  -mu/x + std::log(x)  );
+        return x * std::exp(-mu/x);
     }
 
-    /*
-     * Numerically stable implementation of the first
-     * derivative of x*exp(-mu/x) by x for x in [0,1].
-     */
+    // The first derivative of x*exp(-mu/x) by x.
     inline real_t dSecDecInternalXExpMinusMuOverXd1(real_t mu, real_t x)
     {
         if (x == 0)
             return 0;
 
-        return std::exp(-mu/x) + mu * std::exp(  -mu/x - std::log(x)  );
+        real_t minus_mu_over_x = -mu/x;
+        return std::exp(minus_mu_over_x) - minus_mu_over_x * std::exp(minus_mu_over_x);
     }
 
-    /*
-     * Numerically stable implementation of exp(-mu/x)
-     * for x in [0,1].
-     */
     inline real_t SecDecInternalExpMinusMuOverX(real_t mu, real_t x)
     {
         if (x == 0)
             return 0;
+
         return std::exp(-mu/x);
     }
 
