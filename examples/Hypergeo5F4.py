c635d80dc70d1d18fd84bf1d3ec51fde76d1dc07
Gudrun Heinrich <gudrun@mpp.mpg.de>
2016-11-11 13:49:40 +0100

    prefactor in Hypergeo5F4.py corrected

--- examples/Hypergeo5F4.py
+++ examples/Hypergeo5F4.py
@@ -5,18 +5,18 @@ from pySecDec import make_package
 
 make_package(
 
-name='Hyp5F4',
+name='Hyp5F4_symprefac',
 integration_variables = ['z%i' % i for i in range(4)],
 
 # the order here defines the order of the expansion
 regulators = ['eps'],
 real_parameters = ['b'],
 #functions = [],
-#prefactor = '''gamma(2*eps)*gamma(4*eps)*gamma(6*eps)*gamma(8*eps)/
-#               gamma(-eps)*gamma(-3*eps)*gamma(-5*eps)*gamma(-7*eps)/ 
-#	       gamma(3*eps)/gamma(7*eps)/gamma(11*eps)/gamma(15*eps)''',
+prefactor = '''gamma(2*eps)*gamma(4*eps)*gamma(6*eps)*gamma(8*eps)/
+               (gamma(-eps)*gamma(-3*eps)*gamma(-5*eps)*gamma(-7*eps))/ 
+	       gamma(3*eps)/gamma(7*eps)/gamma(11*eps)/gamma(15*eps)''',
 	       
-prefactor = '121275/128*eps**4',	       
+#prefactor = '121275/128*eps**4',	       
 
 	       
 polynomials_to_decompose = ['z0**(-1-7*eps)','z1**(-1-5*eps)','z2**(-1-3*eps)','z3**(-1-eps)', '(1-z0)**(-1+15*eps)','(1-z1)**(-1+11*eps)','(1-z2)**(-1+7*eps)','(1-z3)**(-1+3*eps)','(1-b*z0*z1*z2*z3)**(-eps)'], 
