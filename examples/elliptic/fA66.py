c8532563c0c44de582d91ed0e56ad7f8ab264ae5
Gudrun Heinrich <gudrun@mpp.mpg.de>
2017-03-07 11:43:56 +0100

    [examples] added elliptic example fA66 fom 1609.06685

---
+++ examples/elliptic/fA66.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python
+from pySecDec.loop_integral import loop_package
+import pySecDec as psd
+
+
+li = psd.loop_integral.LoopIntegralFromPropagators(
+propagators = ['k1**2-msq','(k1+p1+p2)**2-msq','k2**2-msq','(k2+p1+p2)**2-msq','(k1+p1)**2-msq','(k1-k2)**2','(k2-p3)**2-msq','(k2+p1)**2','(k1-p3)**2'],
+loop_momenta = ['k1','k2' ],
+powerlist = [1,1,0,1,1,1,1,0,0],
+external_momenta = ['p1','p2','p3','p4'],
+
+replacement_rules = [
+                        ('p1*p1', 0),
+                        ('p2*p2', 0),
+                        ('p3*p3', 0),
+                        ('p4*p4', 's4'),
+                        ('p1*p2', 's/2'),
+                        ('p2*p3', 't/2'),
+                        ('p1*p3', 's4/2-s/2-t/2')
+                    ]
+
+)
+
+
+Mandelstam_symbols = ['s', 't', 's4']
+mass_symbols = ['msq']
+
+
+loop_package(
+
+name = 'fA66',
+
+loop_integral = li,
+
+real_parameters = Mandelstam_symbols + mass_symbols,
+
+additional_prefactor = 'eps**4*(-s/msq)**(3/2)', 
+
+# the highest order of the final epsilon expansion --> change this value to whatever you think is appropriate
+requested_order = 4,
+
+# the optimization level to use in FORM (can be 0, 1, 2, 3)
+form_optimization_level = 2,
+
+# the WorkSpace parameter for FORM
+form_work_space = '100M',
+
+# the method to be used for the sector decomposition
+# valid values are ``iterative`` or ``geometric`` or ``geometric_ku``
+decomposition_method = 'iterative',
+# if you choose ``geometric[_ku]`` and 'normaliz' is not in your
+# $PATH, you can set the path to the 'normaliz' command-line
+# executable here
+#normaliz_executable='/path/to/normaliz',
+
+# whether or not to produce code to perform the contour deformation
+# if ``True``, it can still be deactivated in the "config.hpp"
+# if ``False``, no code for the contour deformation is generated
+contour_deformation = False,
+
+)
---
+++ examples/elliptic/integrate_fA66.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+from pySecDec.integral_interface import IntegralLibrary
+import sympy as sp
+
+# load c++ library
+fA66 = IntegralLibrary('fA66/fA66_pylink.so')
+
+# choose integrator
+triangle.use_Vegas(flags=2) # ``flags=2``: verbose --> see Cuba manual
+
+# integrate
+str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = triangle(real_parameters=[-4/3,-16/3,-100/39,1])
+
+# convert complex numbers from c++ to sympy notation
+str_integral_with_prefactor = str_integral_with_prefactor.replace(',','+I*')
+str_prefactor = str_prefactor.replace(',','+I*')
+str_integral_without_prefactor = str_integral_without_prefactor.replace(',','+I*')
+
+# convert result to sympy expressions
+integral_with_prefactor = sp.sympify(str_integral_with_prefactor.replace('+/-','*value+error*'))
+integral_with_prefactor_err = sp.sympify(str_integral_with_prefactor.replace('+/-','*value+error*'))
+prefactor = sp.sympify(str_prefactor)
+integral_without_prefactor = sp.sympify(str_integral_without_prefactor.replace('+/-','*value+error*'))
+integral_without_prefactor_err = sp.sympify(str_integral_without_prefactor.replace('+/-','*value+error*'))
+
+# examples how to access individual orders
+print('Numerical Result')
+print('eps^0:', integral_with_prefactor.coeff('eps',0).coeff('value'), '+/- (', integral_with_prefactor_err.coeff('eps',0).coeff('error'), ')')
+print('eps^1:', integral_with_prefactor.coeff('eps',1).coeff('value'), '+/- (', integral_with_prefactor_err.coeff('eps',1).coeff('error'), ')')
+print('eps^2:', integral_with_prefactor.coeff('eps',2).coeff('value'), '+/- (', integral_with_prefactor_err.coeff('eps',2).coeff('error'), ')')
+print('eps^3:', integral_with_prefactor.coeff('eps',3).coeff('value'), '+/- (', integral_with_prefactor_err.coeff('eps',3).coeff('error'), ')')
+print('eps^4:', integral_with_prefactor.coeff('eps',4).coeff('value'), '+/- (', integral_with_prefactor_err.coeff('eps',4).coeff('error'), ')')
+
