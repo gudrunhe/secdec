625c559453a2f3919c59aaa580825661da23b189
Stephan Jahn <sjahn@mpp.mpg.de>
2017-03-16 17:07:23 +0100

    fix hypergeometric example

--- examples/Hypergeo5F4/Hypergeo5F4.py
+++ examples/Hypergeo5F4/Hypergeo5F4.py
@@ -13,13 +13,13 @@ regulators = ['eps'],
 real_parameters = ['b'],
 #functions = [],
 prefactor = '''gamma(2*eps)*gamma(4*eps)*gamma(6*eps)*gamma(8*eps)/
-               (gamma(-eps)*gamma(-3*eps)*gamma(-5*eps)*gamma(-7*eps))/ 
-	       gamma(3*eps)/gamma(7*eps)/gamma(11*eps)/gamma(15*eps)''',
-	       
+               (gamma(-eps)*gamma(-3*eps)*gamma(-5*eps)*gamma(-7*eps))/
+           gamma(3*eps)/gamma(7*eps)/gamma(11*eps)/gamma(15*eps)''',
+
 
 #prefactor = '947.4609375*eps**4 - 286765.9822507143*eps**6 + 1.4350164700988792*^6*eps**7 + 2.668449372562483*^7*eps**8',
-	       
-polynomials_to_decompose = ['z0**(-1-7*eps)','z1**(-1-5*eps)','z2**(-1-3*eps)','z3**(-1-eps)', '(1-z0)**(-1+15*eps)','(1-z1)**(-1+11*eps)','(1-z2)**(-1+7*eps)','(1-z3)**(-1+3*eps)','(1-b*z0*z1*z2*z3)**(-eps)'], 
+
+polynomials_to_decompose = ['z0**(-1-7*eps)','z1**(-1-5*eps)','z2**(-1-3*eps)','z3**(-1-eps)', '(1-z0)**(-1+15*eps)','(1-z1)**(-1+11*eps)','(1-z2)**(-1+7*eps)','(1-z3)**(-1+3*eps)','(1-b*z0*z1*z2*z3)**(-eps)'],
 #remainder_expression = '',
 
 # the highest orders of the final regulator expansion
@@ -32,7 +32,8 @@ form_optimization_level = 2,
 # the WorkSpace parameter for FORM
 form_work_space = '500M',
 
-split = 'True',
+split = True,
+
 )
 
 # analytic result (obtained using HypExp, T.Huber, D.Maitre, hep-ph/0507094)
