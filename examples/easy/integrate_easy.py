c5e123f767cc16032533f53fedbb98211587ae02
Stephan Jahn <sjahn@mpp.mpg.de>
2017-04-20 15:03:13 +0200

    [examples] add “easy”

---
+++ examples/easy/integrate_easy.py
@@ -0,0 +1,12 @@
+from pySecDec.integral_interface import IntegralLibrary
+from math import log
+
+# load c++ library
+easy_integral = IntegralLibrary('easy/easy_pylink.so')
+
+# integrate
+_, _, result = easy_integral()
+
+# print result
+print('Numerical Result:' + result)
+print('Analytic Result:' + ' + (%f)*eps^-1 + (%f) + O(eps)' % (1.0,1.0-log(2.0)))
