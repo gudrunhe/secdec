e440e2119c75c09bb7c24c5d3a025f227f4b3fa4
Stephan Jahn <sjahn@mpp.mpg.de>
2017-06-20 20:06:04 +0200

    [doc] remove hepforge workaround since we host on readthedocs now

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,9 +23,6 @@ sys.path.insert(0, os.path.abspath('../../'))
 
 # -- General configuration ------------------------------------------------
 
-def setup(app):
-    add_undo_hepforge_css(app)
-
 # If your documentation needs a minimal Sphinx version, state it here.
 needs_sphinx = '1.5.5'
 
@@ -115,10 +112,7 @@ modindex_common_prefix = ['pySecDec.']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'bizstyle'
-# Include an additional css file that removes format breaking options
-# introduced by hepforge.
-add_undo_hepforge_css = lambda app: app.add_stylesheet('undo_hepforge.css')
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
