416e03430ebb96440fb43f44a158d7d4d8909a4e
Stephan Jahn <sjahn@mpp.mpg.de>
2017-07-01 14:19:57 +0200

    [high_level_tests] add test case "userdefined_cpp"

---
+++ high_level_tests/userdefined_cpp/generate_userdefined_cpp.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+import shutil
+from pySecDec import make_package
+
+name = 'userdefined_cpp'
+
+make_package(
+
+name=name,
+integration_variables = ['z%i' % i for i in range(4)],
+
+# the order here defines the order of the expansion
+regulators = ['eps'],
+real_parameters = ['alpha'],
+functions = ['dum1', 'dum2'],
+#
+polynomials_to_decompose = ['(z0+z1)**(-2-2*eps)', 'z2**(-1-4*eps)'],
+remainder_expression = '(dum1(z0,z1,z2,z3) + 5*eps*z0)**(1+eps) * dum2(z0,z1,alpha)**(2-6*eps)',
+#remainder_expression = '(z0**2+z1**3+z2**4+z3**5 + 4*z0*z1*z2*z3+2-z0**2*z1**3*z2**4*z3**5 + 5*eps*z0)**(1+eps) * (z0**2 + z1**2 +alpha**2 + 4*z0*z1+3*z0**2*z1**2 - sqrt(z0*z1*alpha))**(2-6*eps)',
+
+
+# the highest orders of the final regulator expansion
+# the order here matches the order of ``regulators``
+requested_orders = [1],
+
+# the optimization level to use in FORM (can be 0, 1, 2, 3)
+form_optimization_level = 2,
+
+# the WorkSpace parameter for FORM
+form_work_space = '500M',
+
+)
+
+# check the generated "functions.hpp"
+with open(name + '/src/functions.hpp') as generated_header:
+    with open('functions_template.hpp') as target_header:
+        assert generated_header.read() == target_header.read(), 'mismatch between generated and expected "functions.hpp"'
+
+# copy 'functions.hpp' (predefined for this example) to required directory
+shutil.copy('functions_implementation.hpp',name+'/src/functions.hpp')
---
+++ high_level_tests/userdefined_cpp/test/test.py
@@ -0,0 +1,82 @@
+from __future__ import print_function
+from pySecDec.integral_interface import IntegralLibrary
+import sympy as sp
+import unittest
+
+class CheckLib(unittest.TestCase):
+    def setUp(self):
+        # load c++ library
+        self.lib = IntegralLibrary('../userdefined_cpp/userdefined_cpp_pylink.so')
+        self.target_result = {
+                                 -2:   0.095206,
+                                 -1:  -2.561,
+                                  0:  21.120,
+                                  1: -78.7
+                             }
+        self.real_parameters = [0.77]
+        self.epsrel = 1e-3
+        self.maxeval = 10**8
+
+    def check_result(self, computed_series, epsrel):
+        # convert result to sympy expressions
+        integral_with_prefactor = sp.sympify(  computed_series.replace(',','+I*').replace('+/-','*value+error*')  )
+
+        for order in range(-2,2):
+            value = complex( integral_with_prefactor.coeff('eps',order).coeff('value') )
+            error = complex( integral_with_prefactor.coeff('eps',order).coeff('error') )
+
+            # check that the uncertainties are reasonable
+            self.assertLessEqual(error.real, abs(2*epsrel * self.target_result[order].real))
+            self.assertLessEqual(error.imag, abs(2*epsrel * self.target_result[order].imag))
+
+            # check that the desired uncertainties are reached
+            self.assertLessEqual(error.real, abs(epsrel * value.real) )
+            self.assertLessEqual(error.imag, abs(epsrel * value.imag) )
+
+            # check integral value
+            self.assertAlmostEqual(  value.real, self.target_result[order].real, delta=epsrel*abs(self.target_result[order].real)  )
+            self.assertAlmostEqual(  value.imag, self.target_result[order].imag, delta=epsrel*abs(self.target_result[order].imag)  )
+
+    def test_Vegas(self):
+        # choose integrator
+        self.lib.use_Vegas(flags=2, epsrel=self.epsrel, maxeval=self.maxeval) # ``flags=2``: verbose --> see Cuba manual
+
+        # integrate
+        str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = self.lib(self.real_parameters)
+
+        # check
+        self.check_result(str_integral_with_prefactor, self.epsrel)
+
+    def test_Suave(self):
+        # choose integrator
+        self.lib.use_Suave(flags=2, epsrel=self.epsrel, maxeval=self.maxeval) # ``flags=2``: verbose --> see Cuba manual
+
+        # integrate
+        str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = self.lib(self.real_parameters)
+
+        # check
+        self.check_result(str_integral_with_prefactor, self.epsrel)
+
+    def test_Divonne(self):
+        # choose integrator
+        self.lib.use_Divonne(flags=2, epsrel=self.epsrel, border=1e-8, maxeval=self.maxeval) # ``flags=2``: verbose --> see Cuba manual
+
+        # integrate
+        str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = self.lib(self.real_parameters)
+
+        # check
+        self.check_result(str_integral_with_prefactor, self.epsrel)
+
+    def test_Cuhre(self):
+        # choose integrator
+        # Note: Need `mineval` because Cuhre underestimates the error
+        self.lib.use_Cuhre(flags=2, epsrel=self.epsrel, maxeval=self.maxeval, mineval=6*10**4) # ``flags=2``: verbose --> see Cuba manual
+
+        # integrate
+        str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = self.lib(self.real_parameters)
+
+        # check
+        self.check_result(str_integral_with_prefactor, self.epsrel)
+
+if __name__ == '__main__':
+    unittest.main()
