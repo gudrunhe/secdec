2ea86eaa7e46f2d9809704dd18eaa79feb2299e5
Stephan Jahn <sjahn@mpp.mpg.de>
2017-09-22 14:42:05 +0200

    [examples/userdefined_cpp] update comments in edited "functions.hpp" files
    This is a fixup of dedfd90d452fa43d5404345ba35b4d14146449fe.

--- examples/userdefined_cpp/functions_dummyI.hpp
+++ examples/userdefined_cpp/functions_dummyI.hpp
@@ -12,7 +12,9 @@ namespace dummyI
 
     /*
      * Declarations of the `functions` and their required
-     * derivatives are declared here. You can either add "inline"
+     * derivatives are declared here. The derivative of a function
+     * 'f' by its i-th argument is denoted as 'dfdi'. To implement
+     * the functions listed below, you can either add "inline"
      * keywords and define these functions here, or you define the
      * functions in a separate '.cpp' file. If you decide for a
      * separate file, the file name can be arbitrary up to the
@@ -21,6 +23,12 @@ namespace dummyI
      * an external library (e.g. the gsl), you should add the
      * corresponding compiler and linker flags to the "Makefile.conf"
      * in the top level directory.
+     *
+     * Note: Not all functions listed here may actually be needed.
+     *       This file lists all derivatives that occurred in the
+     *       calculation. It is possible that some dropped out due
+     *       to algebraic simplifications after this list was
+     *       generated.
      */
 
 
