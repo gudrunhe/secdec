1ee085d1aa0b17f08e3631531a11694550b249d7
Stephan Jahn <sjahn@mpp.mpg.de>
2018-03-01 12:26:17 +0100

    [cumc] draft qmc high-level test cases

---
+++ high_level_tests/one_integration_variable/test/test.cpp
@@ -0,0 +1,88 @@
+#include "catch.hpp"
+
+#include <complex> // std::complex
+#include <numeric> // std::accumulate
+#include <secdecutil/integrators/integrator.hpp> // MultiIntegrator
+#include <secdecutil/integrators/cuba.hpp> // Vegas
+#include <secdecutil/integrators/cquad.hpp> // CQuad
+#include <secdecutil/deep_apply.hpp> // deep_apply
+#include <secdecutil/series.hpp> // Series
+
+#define QUOTE(ARG) #ARG
+#define QUOTE_EXPAND(ARG) QUOTE(ARG)
+#define INTEGRAL_NAME bubble1L_dot_rank4
+
+#include QUOTE_EXPAND(INTEGRAL_NAME.hpp)
+
+TEST_CASE( "check result", "[INTEGRAL_NAME]" ) {
+
+    // User Specified Phase-space point
+    const std::vector<INTEGRAL_NAME::real_t> real_parameters = { 1.275, 1.275 };
+    const std::vector<INTEGRAL_NAME::complex_t> complex_parameters = { 30.886875, 30.886875, 123.5475 };
+
+    // get the integrands
+    const auto sector_integrands = INTEGRAL_NAME::make_integrands(real_parameters, complex_parameters);
+
+    // add integrands of sectors (together flag)
+    const auto all_sectors = std::accumulate(++sector_integrands.begin(), sector_integrands.end(), *sector_integrands.begin() );
+
+    // define and configure integrator
+    secdecutil::gsl::CQuad<INTEGRAL_NAME::integrand_return_t> cquad;
+    secdecutil::cuba::Cuhre<INTEGRAL_NAME::integrand_return_t> cuhre;
+    cuhre.maxeval = 1e7;
+    cuhre.together = true;
+    const double epsrel = 1e-4; cquad.epsrel = cuhre.epsrel = epsrel;
+    const double epsabs = 1e-7; cquad.epsabs = cuhre.epsabs = epsabs;
+    secdecutil::MultiIntegrator<INTEGRAL_NAME::integrand_return_t,INTEGRAL_NAME::real_t> integrator(cquad,cuhre,2);
+
+    // integrate
+    auto result_without_prefactor = secdecutil::deep_apply( all_sectors,  integrator.integrate );
+    auto prefactor = INTEGRAL_NAME::prefactor(real_parameters, complex_parameters);
+    auto result_with_prefactor = result_without_prefactor * prefactor;
+
+
+    // target result, obtained in a long run of pySecDec
+    constexpr std::complex<double> I(0,1);
+    secdecutil::Series<std::complex<double>> target_result_with_prefactor
+    (
+
+        -1, // lowest order in epsilon
+         0, // highest computed order in epsilon
+
+        {
+             1.0    + 0.0    * I, // eps ** -1
+            -1.2708 + 2.4179 * I, // eps **  0
+        },
+
+        true, // series is truncated above; i.e. "+ O(eps**2)"
+        "eps" // the expansion parameter
+    );
+
+    // basic checks
+    REQUIRE(          result_with_prefactor.get_order_min() == target_result_with_prefactor.get_order_min()          );
+    REQUIRE(          result_with_prefactor.get_order_max() == target_result_with_prefactor.get_order_max()          );
+    REQUIRE(    result_with_prefactor.get_truncated_above() == target_result_with_prefactor.get_truncated_above()    );
+    REQUIRE(      result_with_prefactor.expansion_parameter == target_result_with_prefactor.expansion_parameter      );
+
+    std::cout << "----------------" << std::endl << std::endl;
+
+    for (int order = target_result_with_prefactor.get_order_min() ; order <= target_result_with_prefactor.get_order_max() ; ++ order)
+    {
+        std::cout << "checking order \"eps^" << order << "\" ..." << std::endl;
+
+        // check that the uncertainties are reasonable
+        REQUIRE(      result_with_prefactor.at(order).uncertainty.real() <= std::abs(2*epsrel * target_result_with_prefactor.at(order).real())      );
+        if (  target_result_with_prefactor.at(order).imag() != 0.0  )
+            REQUIRE(      result_with_prefactor.at(order).uncertainty.imag() <= std::abs(2*epsrel * target_result_with_prefactor.at(order).imag())      );
+
+        // check values
+        REQUIRE(  result_with_prefactor.at(order).value.real() == Approx( target_result_with_prefactor.at(order).real() ).epsilon( 10.0*epsrel )  );
+        if (  target_result_with_prefactor.at(order).imag() == 0.0  )
+            REQUIRE(  result_with_prefactor.at(order).value.imag() <= epsabs  );
+        else
+            REQUIRE(  result_with_prefactor.at(order).value.imag() == Approx( target_result_with_prefactor.at(order).imag() ).epsilon( 10.0*epsrel )  );
+
+        std::cout << "----------------" << std::endl << std::endl;
+    }
+
+};
---
+++ high_level_tests/one_integration_variable_cuda/generate_one_integration_variable.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+import shutil
+from pySecDec import make_package
+
+name = 'one_integration_variable'
+
+make_package(
+
+name=name,
+integration_variables = ['x'],
+
+# the order here defines the order of the expansion
+regulators = ['eps'],
+
+# the highest orders of the final regulator expansion
+# the order here matches the order of ``regulators``
+requested_orders = [1],
+
+polynomials_to_decompose = ['(x)**(-1+eps)'],
+polynomial_names = ['p'],
+other_polynomials = ['p**-2'],
+)
