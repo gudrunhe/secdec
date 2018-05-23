139c904ba46000be0c791ee727d3e5265c68fe3c
Sophia Carola Borowka <sophia.borowka@cern.ch>
2018-05-23 16:25:44 +0200

    Added pentabox example.

---
+++ nodist_examples/pentabox/integrate_pentabox.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+from pySecDec.integral_interface import IntegralLibrary
+import sympy as sp
+
+# load c++ library
+pentabox = IntegralLibrary('pentabox/pentabox_pylink.so')
+
+# choose integrator
+pentabox.use_Vegas(flags=2,epsrel=1e-3,maxeval=10**7)
+
+ 
+# integrate non-Euclidean point;
+s12, s23, s34, s45, s51 = [5.,-4.,2.,-6.,3.] 
+str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = pentabox([s12, s23, s34, s45, s51],deformation_parameters_maximum = 0.1)
+
+# convert complex numbers from c++ to sympy notation
+str_integral_with_prefactor = str_integral_with_prefactor.replace(',','+I*')
+str_prefactor = str_prefactor.replace(',','+I*')
+str_integral_without_prefactor = str_integral_without_prefactor.replace(',','+I*')
+
+# convert result to sympy expressions
+integral_with_prefactor = sp.sympify(str_integral_with_prefactor.replace('+/-','*value+error*'))
+integral_with_prefactor_err = sp.sympify(str_integral_with_prefactor.replace('+/-','*value+error*'))
+prefactor = sp.sympify(str_prefactor)
+integral_without_prefactor = sp.sympify(str_integral_without_prefactor.replace('+/-','*value+error*'))
+integral_without_prefactor_err = sp.sympify(str_integral_without_prefactor.replace('+/-','*value+error*'))
+
+# numerical result
+print('eps^0:', integral_with_prefactor.coeff('eps',0).coeff('value'), '+/- (', integral_with_prefactor_err.coeff('eps',0).coeff('error'), ')')
+
+# using epsrel=1.e-4 the 
+# result gives:eps^0: -0.0198270127732515788 - 0.034157996078159697*I +/- ( 0.00001693731512749778 + 0.0000132963931427676128*I )
