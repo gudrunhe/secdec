b866092e64aba09caf04cb9d68bab8fa051cd728
Stephan Jahn <sjahn@mpp.mpg.de>
2018-06-15 19:20:26 +0200

    [examples.formfactor4L] adapt integrate_<...>.py to get more digits
    It turns out that the tent transform is better for this integral than any
    Korobov transform.

--- nodist_examples/formfactor4L/integrate_formfactor4L.py
+++ nodist_examples/formfactor4L/integrate_formfactor4L.py
@@ -8,19 +8,21 @@ ff = IntegralLibrary('formfactor4L/formfactor4L_pylink.so')
 # choose integrator
 #ff.use_Vegas(flags=2, maxeval=10**6, mineval=10**6)
 ff.use_Qmc(
-minn=15*10**7,
+minn=35*10**5,
 minm = 64,
 
-cudablocks=512, cudathreadsperblock=256,
-maxnperpackage=64, maxmperpackage=2**10,
+cudablocks=128, cudathreadsperblock=64,
+maxnperpackage=64, maxmperpackage=32,
 
-epsrel=1e-100, epsabs=1e-10, maxeval=10**12,
+#epsrel=1e-100, epsabs=1e-10, maxeval=10**12,
 
-verbosity=2,
+verbosity=3,
+
+transform='tent',
 )
 
 # integrate
-str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = ff(together=False)
+str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = ff(together=True)
 
 print('Numerical Result')
 print(str_integral_without_prefactor)
