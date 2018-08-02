28af474e5e3a20759342abc6da0cb6a7ccd97548
Stephen Jones <sjones@mpp.mpg.de>
2018-08-02 19:37:32 +0200

    [nodist_examples] add example `formfactor1L_expansion_by_regions`

---
+++ nodist_examples/formfactor1L_expansion_by_regions/generate_formfactor1L_massive_expansion_by_regions.py
@@ -0,0 +1,27 @@
+from pySecDec.region_expand import make_regions
+
+# Example 6 of Bernd Jantzen (arXiv:1111.2589)
+make_regions(
+
+# make_regions_args
+name = 'formfactor1L_massive_expansion_by_regions',
+integration_variables = ['x0','x1','x2'],
+regulators = ['n0','n1','eps'],
+requested_orders = [0,0,0],
+smallness_parameter = 'z',
+polynomials_to_decompose = ['(x0+x1+x2)**((1+n0)+(1+n1)-3+2*eps)','((z**2*msq)*x2*(x0+x1+x2)+(qsq)*x0*x1)**(-(1+n0)-(1+n1)+1-eps)','x0**n0','x1**n1'],
+expansion_by_regions_order = 0,
+real_parameters = ['qsq','msq'],
+complex_parameters = [],
+
+# make_package_args
+polynomial_names = ['U','F'],
+contour_deformation_polynomial = 'F',
+positive_polynomials = ['U'],
+prefactor = '-gamma(eps + 1)', # limits n0,n1->0 already taken in the prefactor
+decomposition_method = 'geometric'
+)
+
+# Expected result from Eq(6.30) of arXiv:1111.2589
+# N[-1/qsq*(1/2*Log[qsq/msq]^2 + Log[qsq/msq]*Log[1 - msq/qsq] - PolyLog[2, msq/qsq] + Pi^2/3) /. {qsq -> 100, msq -> 1}, 20]
+# -0.13837355735881397785 + O(eps) + O(n1)
---
+++ nodist_examples/formfactor1L_expansion_by_regions/generate_formfactor1L_massless_expansion_by_regions.py
@@ -0,0 +1,26 @@
+from pySecDec.region_expand import make_regions
+
+# Section 3.1 (Method of Regions for the Sudakov form factor) of Thomas Becher (arXiv:1803.04310)
+make_regions(
+
+# make_regions_args
+name = 'formfactor1L_massless_expansion_by_regions',
+integration_variables = ['x0','x1','x2'],
+regulators = ['eps'],
+requested_orders = [0],
+smallness_parameter = 'z',
+polynomials_to_decompose = ['( + (1)*x0 + (1)*x1 + (1)*x2)**(2*eps - 1)','( + (z**2*lsq)*x1*x2 + (z**2*psq)*x0*x2 + (qsq)*x0*x1)**(-eps - 1)'],
+expansion_by_regions_order = 0,
+real_parameters = ['qsq','lsq','psq'],
+complex_parameters = [],
+
+# make_package_args
+polynomial_names = ['U','F'],
+contour_deformation_polynomial = 'F',
+positive_polynomials = ['U'],
+prefactor = '-gamma(eps + 1)', # non-singular limits 1+n0->1 and 1+n1->1 already taken in prefactor
+decomposition_method = 'iterative'
+)
+
+# Expected Result for qsq=100, lsq=1, psq=1 (Compared with Long Chen)
+# + ((0,0) +/- (2.93878e-19,0))*eps^-2 + ((-1.6865e-11,8.49456e-11) +/- (1.25602e-07,7.58591e-08))*eps^-1 + ((-0.244975,3.27442e-10) +/- (1.20291e-06,1.07642e-06)) + O(eps)
