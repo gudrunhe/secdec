c41deb98e44834dc8e4af37c5c8ff17c0b578c72
Stephan Jahn <sjahn@mpp.mpg.de>
2018-08-02 18:52:39 +0200

    update changelog, update pySecDec version number
    the version of the util is not updated because there are no changes in the
    util compared to version 1.3.1

--- pySecDec/metadata.py
+++ pySecDec/metadata.py
@@ -1,4 +1,4 @@
-version = __version__ = '1.3.1'
+version = __version__ = '1.3.2'
 authors = __authors__ = 'Sophia Borowka, Gudrun Heinrich, Stephan Jahn, Stephen Jones, Matthias Kerner, Johannes Schlenk, Tom Zirke'
 
 from subprocess import check_output as shell_call
