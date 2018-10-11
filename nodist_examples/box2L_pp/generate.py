a8f673162eff8958ce4799eb5f12e8658c99bf82
Stephan Jahn <sjahn@mpp.mpg.de>
2018-10-11 14:06:53 +0200

    [nodist_examples] merge sum_reduze into box2L_pp

--- nodist_examples/box2L_pp/generate.py
+++ nodist_examples/box2L_pp/generate.py
@@ -1,6 +1,7 @@
 from __future__ import division
+from pySecDec.code_writer.sum_package import sum_package, Coefficient
+from pySecDec.loop_integral import loop_package
 
-from argparse import ArgumentParser
 from fractions import Fraction
 import pySecDec as psd
 import itertools as it
@@ -9,24 +10,14 @@ import sympy as sp
 import os
 import shutil
 
-parser = ArgumentParser()
-
-parser.add_argument("-i", "--intfile", dest="intfile",
-                    action="store", type=str, required=True,
-                    help='the Reduze file to process',
-                    metavar="INTFILE")
-
-args = parser.parse_args()
-
-
-kinematic_point = {}
-kinematic_point["s"] = '(405/100)'
-kinematic_point["t"] = '(-2025/1000)'
-kinematic_point["msq"] = '1'
+#User Input
+name_reduze_file = 'F3_121111100.red'
 
 mandelstam_symbols = ['s','t']
 mass_symbols = ['msq']
 additional_prefactor = 'eps**4*(s/msq)**2'
+contour_deformation = True
+requested_order = 3
 
 loop_momenta = ['p1','p2']
 external_momenta = ['k1','k2','k3']
@@ -44,19 +35,23 @@ families["AA_F1"] = ['(p1)**2-msq','(p2)**2-msq','(p1-p2)**2','(p1+k1)**2-msq','
 families["AA_F1x123"] = ['(p1)**2-msq','(p2)**2-msq','(p1-p2)**2','(p1+k2)**2-msq','(p2+k2)**2-msq','(p1-k3)**2-msq','(p2-k3)**2-msq','(p1-k3-k1)**2-msq','(p2-k3-k1)**2-msq']
 families["AA_F3"] = ['(p1)**2','(p1-p2)**2-msq','(p1+k1)**2','(p2+k1)**2-msq','(p1-k2)**2','(p2-k2)**2-msq','(p2-k2-k3)**2-msq','(p1-k2-k3)**2','(p2)**2-msq']
 
-def parse_integral(line):
-
+def parse_integral(line,families):
+    ''' Extracts the name, integral family, dimensions and powerlist from \
+        a line that was identified as an integral. Returns a dict with \
+        the arguments for loop_package '''
     integral_pattern = re.compile(r"(\S*)\s*[0-9]*\s*[0-9]*\s*[0-9]*\s*[0-9]*\s*(.*)", re.IGNORECASE)
     dim_pattern = re.compile('dim(inc|dec)(\d+)', re.IGNORECASE)
 
     # Get integral name and power list
     integral_match = integral_pattern.findall(line)
     name, powerlist = integral_match[0][0], map(int, integral_match[0][1].split())
-
     # Parse dimension shift
     dim = 4
-    dim_match =  dim_pattern.findall(name)
-    name = dim_pattern.sub('',name)
+    dim_match = dim_pattern.findall(name)
+
+    name_family = dim_pattern.sub('',name)
+    name = name + '_' + integral_match[0][1].replace(' ','_').replace('-','m')
+
     if dim_match:
         assert len(dim_match) == 1
         if dim_match[0][0] == 'inc':
@@ -66,120 +61,72 @@ def parse_integral(line):
         else:
             raise "Could not parse dimension shift for integral %s" % name
 
-    print(line, name, dim, powerlist)
+    li = psd.loop_integral.LoopIntegralFromPropagators(
+            loop_momenta=loop_momenta,
+            external_momenta=external_momenta,
+            propagators=families[name_family],
+            replacement_rules=replacement_rules,
+            powerlist=powerlist,
+            dimensionality=str(dim) + "-2*eps"
+        )
 
-    return name, dim, powerlist
+    #Define the package_args dict with all arguments for loop_package
+    package_args = {}
+    package_args['name'] = name
+    package_args['loop_integral'] = li
+    #The requested order can not be calculated yet as it depends on the order of the coefficient
+    package_args['real_parameters'] = mandelstam_symbols+mass_symbols
+    package_args['contour_deformation'] = contour_deformation
+    package_args['additional_prefactor'] = additional_prefactor
+
+    return package_args
 
 def parse_coefficient(line, dimension_symbol):
-
-    # TODO - should be independent of kinematics (automate this)
-    # Insert d -> 4-2*eps using Sympy and split numerator/denominator (Slow)
+    '''Decomposes the coefficent into numerator and denominator and \
+    calculates the order of epsilon of the coefficient.'''
     d = sp.symbols(dimension_symbol)
     eps = sp.symbols("eps")
-    s = sp.symbols("s")
-    t = sp.symbols("t")
-    coeff_n, coeff_d = sp.fraction(sp.cancel(sp.together(sp.sympify(line).subs(d,4-2*eps).subs(s,kinematic_point["s"]).subs(t,kinematic_point["t"])))) #TODO - mass
-    coeff_n = psd.algebra.Expression(coeff_n,['eps']).simplify()
-    coeff_d_poly = psd.algebra.Expression(coeff_d,['eps']).simplify()
-    coeff_d = psd.algebra.ExponentiatedPolynomial(coeff_d_poly.expolist,coeff_d_poly.coeffs,exponent=-1,polysymbols=coeff_d_poly.polysymbols)
+    coeff_n, coeff_d = sp.fraction(sp.cancel(sp.together(sp.sympify(line).subs(d,4-2*eps))))
+    coefficient = Coefficient([str(coeff_n)],[str(coeff_d)],[eps],mandelstam_symbols+mass_symbols)
+    return coefficient
 
-    # Compute Laurent expansion of coefficient
-    coeff = psd.algebra.Product(coeff_n,coeff_d)
-    coeff = psd.expansion.expand_singular(coeff,0,4)
- 
-    return coeff
+def parse_reduze_file(reduze_file):
 
-integrals = []
-coefficients = []
-with open(args.intfile) as f:
+    list_package_args = []
+    coefficients = []
 
-    for line in f.readlines():
+    with open(reduze_file) as f:
 
-        # Count spaces, indicates the type of line to be parsed
-        spaces = sum(1 for _ in it.takewhile(lambda c: c == ' ', line))
-        line = line.strip()
+        for line in f.readlines():
 
-        if spaces == 0:
-            assert line.strip() == '' or line.strip() == ';'
-        elif spaces == 2:
-            # LHS integral
-            print('LHS',line)
-        elif spaces == 3:
-            assert line.strip() == '1'
-        elif spaces == 4:
-            # RHS integral
-            integral = {}
-            integral["name"], integral["dim"], integral["powerlist"] = parse_integral(line)
-            integrals.append(integral)
+            # Count spaces, indicates the type of line to be parsed
+            spaces = sum(1 for _ in it.takewhile(lambda c: c == ' ', line))
+            line = line.strip()
 
-        elif spaces == 5:
-            # Coefficient
-            coefficients.append(parse_coefficient(line,'d'))
+            if spaces == 0:
+                assert line.strip() == '' or line.strip() == ';'
+            elif spaces == 2:
+                #Get LHS integral name
+                integral_pattern = re.compile(r"(\S*)\s*[0-9]*\s*[0-9]*\s*[0-9]*\s*[0-9]*\s*(.*)", re.IGNORECASE)
+                integral_match = integral_pattern.findall(line)
+                integral_name = integral_match[0][0]+ '_' + integral_match[0][1].replace(' ','_').replace('-','m')
+            elif spaces == 3:
+                assert line.strip() == '1'
+            elif spaces == 4:
+                # RHS integral
+                list_package_args.append(parse_integral(line,families))
 
-assert len(integrals) == len(coefficients)
+            elif spaces == 5:
+                # Coefficient
+                coefficients.append(parse_coefficient(line,'d'))
 
-# Sector decompose integrals and build library
-built_integrals = []
-for integral,coefficient in zip(integrals,coefficients):
+    assert len(list_package_args) == len(coefficients)
 
-    assert integral["name"] in families.keys()
+    # get real_parameters and remove them from package_args
+    real_parameters=list_package_args[0]['real_parameters']
+    for package_args in list_package_args:
+        assert package_args.pop('real_parameters') == real_parameters
 
-    # Prepare for creation of integral directory
-    name = integral["name"] + "_dim" + str(integral["dim"]) + "_" + ''.join(map(str,integral["powerlist"])).replace('-', 'm')
-    shutil.rmtree(name, ignore_errors=True)
+    sum_package(integral_name,[loop_package]*len(list_package_args), list_package_args, ['eps'], [requested_order],real_parameters=real_parameters,coefficients=coefficients)
 
-    # Determine lowest order of eps in coefficient
-    coefficient_min_order = int(min(coefficient.expolist))
-
-    li = psd.loop_integral.LoopIntegralFromPropagators(
-        loop_momenta=loop_momenta,
-        external_momenta=external_momenta,
-        propagators=families[integral["name"]],
-        replacement_rules=replacement_rules,
-        powerlist=integral["powerlist"],
-        dimensionality=str(integral["dim"]) + "-2*eps"
-    )
-
-    evaluated_coefficient = ''
-    for coeff,exponent in zip(coefficient.coeffs,coefficient.expolist):
-        numeric_coefficient = str(coeff)
-        evaluated_coefficient += "+(" + numeric_coefficient + ")*eps**(" + str(exponent[0]) + ")"
-
-    print(name)
-    print(evaluated_coefficient)
-
-    psd.loop_integral.loop_package(
-        name=name,
-        loop_integral=li,
-        real_parameters=mandelstam_symbols+mass_symbols,
-        additional_prefactor='(' + additional_prefactor + ')' + '*(' + evaluated_coefficient + ')',
-        requested_order=4, # TODO - should be requested_order=0 here
-        form_optimization_level=2,
-        form_work_space='1G',
-        decomposition_method='iterative',
-        contour_deformation=True
-    )
-
-    built_integral = {}
-    built_integral["name"] = name
-
-    built_integrals.append(built_integral)
-
-    # Build example
-    for i in range(5):
-        retcode = os.system('CXX=nvcc CPATH=/home/pcl335/sjones/qmc/src make -j20 -C' + name) # TODO - j should be parameter
-        if retcode == 0:
-            break
-        else:
-            print("retrying build...")
-
-with open('results.txt','w') as resultsfile:
-
-    for built_integral in built_integrals:
-        library = psd.integral_interface.IntegralLibrary(built_integral["name"] + "/" + built_integral["name"] + "_pylink.so")
-        library.use_Qmc(devices=[3],epsrel=1e-5, maxeval=10**8)
-        _, _, str_integral_with_prefactor = library([float(Fraction(kinematic_point["s"].lstrip('(').rstrip(')'))), float(Fraction(kinematic_point["t"].lstrip('(').rstrip(')'))), float(Fraction(kinematic_point["msq"].lstrip('(').rstrip(')')))])
-
-        resultsfile.write(str_integral_with_prefactor)
-        print(str_integral_with_prefactor)
-
+parse_reduze_file(name_reduze_file)
---
+++ nodist_examples/box2L_pp/integrate_AA_F3_1_1_1_1_1_1_1_0_0.cpp
@@ -0,0 +1,41 @@
+#include <iostream> // std::cout
+#include <vector> // std::vector
+
+#include <secdecutil/uncertainties.hpp> // secdecutil::UncorrelatedDeviation
+
+#include "AA_F3_1_1_1_1_1_1_1_0_0.hpp"
+
+int main()
+{
+    // User Specified Phase-space point
+    const std::vector<AA_F3_1_1_1_1_1_1_1_0_0::real_t> real_parameters = { -1., -5., 100. };
+    const std::vector<AA_F3_1_1_1_1_1_1_1_0_0::complex_t> complex_parameters = { /* EDIT: insert complex parameter values here */ };
+
+    // Construct the amplitude. Further options for the individual integrals can be set in the
+    // corresponding "<integral_name>_weighted_integral.cpp" file in the "src/" directory
+    AA_F3_1_1_1_1_1_1_1_0_0::nested_series_t<AA_F3_1_1_1_1_1_1_1_0_0::sum_t> unwrapped_amplitude =
+        AA_F3_1_1_1_1_1_1_1_0_0::make_amplitude(real_parameters, complex_parameters);
+
+    // pack amplitude into handler
+    AA_F3_1_1_1_1_1_1_1_0_0::handler_t<AA_F3_1_1_1_1_1_1_1_0_0::nested_series_t> amplitude
+    (
+        unwrapped_amplitude
+        ,1e-10, 1e-20, 2147483647, 1e6 // epsrel, epsabs, maxeval, mineval
+        // further optional arguments: epsrel, epsabs, maxeval, mineval, maxincreasefac, min_epsrel, min_epsabs, max_epsrel, max_epsabs
+    );
+    amplitude.verbose = true;
+
+    // The optional further arguments of the handler are set for all orders.
+    // To specify different settings for a particular order, type e.g.: amplitude.expression.at(0).epsrel = 1e-5;
+
+    // optionally set wall clock limits (in seconds)
+    // Note: Only the wall clock time spent in "amplitude.evaluate()" is considered for these limits.
+    amplitude.soft_wall_clock_limit = 2 * 60 * 60;
+    amplitude.hard_wall_clock_limit = 3 * 60 * 60;
+
+    // compute the amplitude
+    const AA_F3_1_1_1_1_1_1_1_0_0::nested_series_t<secdecutil::UncorrelatedDeviation<AA_F3_1_1_1_1_1_1_1_0_0::integrand_return_t>> result = amplitude.evaluate();
+
+    // print the result
+    std::cout << "amplitude = " << result << std::endl;
+}
