91fffd81ac5c9387c5a8d9ad80731f7e5e21af92
Stephan Jahn <sjahn@mpp.mpg.de>
2018-10-24 17:53:00 +0200

    [polytope] adapt error type and message

--- pySecDec/polytope.py
+++ pySecDec/polytope.py
@@ -354,7 +354,7 @@ class Polytope(object):
             if 'equations' in current_str or re.sub(r'[\n\t\s]*', '', current_str) == '0':
                 return np.fromstring(array_as_str, sep=' ', dtype=int).reshape(len(array_lines),-1)
             else:
-                raise ValueError("Scaleless integral detected. Scaleless integrals evaluate to zero.")
+                raise NotImplementedError("Polytope is not full dimensional. Are you trying to compute a scaleless integral (which evaluates to zero)?")
 
 
     def _read_ext_file(self, filepath):
