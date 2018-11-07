d433b8cb4e2a52aca6a4284f2cabe881f2811919
Stephan Jahn <sjahn@mpp.mpg.de>
2018-11-07 15:16:18 +0100

    [examples] add HZ2L_nonplanar

---
+++ examples/HZ2L_nonplanar/integrate_HZ2L_nonplanar.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+from pySecDec.integral_interface import IntegralLibrary
+import sympy as sp
+
+# load c++ library
+HZ2L_nonplanar = IntegralLibrary('HZ2L_nonplanar/HZ2L_nonplanar_pylink.so')
+
+# choose integrator
+HZ2L_nonplanar.use_Qmc(verbosity=3,minn=10**8,maxeval=10**8,transform='korobov3')
+
+# integrate
+str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = HZ2L_nonplanar(real_parameters=[200.,-23.,9.,1.56,0.81])
+
+# convert complex numbers from c++ to sympy notation
+str_integral_with_prefactor = str_integral_with_prefactor.replace(',','+I*')
+str_prefactor = str_prefactor.replace(',','+I*')
+str_integral_without_prefactor = str_integral_without_prefactor.replace(',','+I*')
+
+# convert result to sympy expressions
+integral_with_prefactor = sp.sympify(str_integral_with_prefactor.replace('+/-','*value+error*'))
+integral_with_prefactor_err = sp.sympify(str_integral_with_prefactor.replace('+/-','*value+error*'))
+prefactor = sp.sympify(str_prefactor)
+integral_without_prefactor = sp.sympify(str_integral_without_prefactor.replace('+/-','*value+error*'))
+integral_without_prefactor_err = sp.sympify(str_integral_without_prefactor.replace('+/-','*value+error*'))
+
+# examples how to access individual orders
+print('Numerical Result')
+print('eps^-2 with prefactor:', integral_with_prefactor.coeff('eps',-2).coeff('value'), '+/- (', integral_with_prefactor_err.coeff('eps',-2).coeff('error'), ')')
+print('eps^-1 with prefactor:', integral_with_prefactor.coeff('eps',-1).coeff('value'), '+/- (', integral_with_prefactor_err.coeff('eps',-1).coeff('error'), ')')
+print('eps^0 with prefactor:', integral_with_prefactor.coeff('eps',0).coeff('value'), '+/- (', integral_with_prefactor_err.coeff('eps',0).coeff('error'), ')')
