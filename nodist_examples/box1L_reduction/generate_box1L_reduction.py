e47b8be60fdfc5dc31ea1aa81638557d6972032a
Gudrun Marlen Heinrich <gudrun@mpp.mpg.de>
2019-07-02 14:48:34 +0200

    [nodist_examples] box1L example added

---
+++ nodist_examples/box1L_reduction/generate_box1L_reduction.py
@@ -0,0 +1,94 @@
+#! /usr/bin/env python
+from pySecDec.code_writer.sum_package import sum_package, Coefficient
+from pySecDec.loop_integral import loop_package, LoopIntegralFromPropagators
+
+Mandelstam_symbols = ['s','t']
+mass_symbols = []
+
+real_parameters = Mandelstam_symbols + mass_symbols
+
+def make_1L_integral(powerlist, dim='4-2*eps'):
+
+    return dict(
+
+        name = 'I_' + '_'.join( map(str,powerlist) ),
+
+        loop_integral = LoopIntegralFromPropagators(
+
+            propagators = ['(p1)**2','(p1+k1)**2','(p1+k1+k2)**2','(p1+k1+k2+k3)**2'],
+            loop_momenta = ['p1'],
+            external_momenta = ['k1','k2','k3','k4'],
+            replacement_rules = [
+                                ('k1*k1', '0'),
+                                ('k2*k2', '0'),
+                                ('k3*k3', '0'),
+                                ('k1*k2', 's/2'),
+                                ('k1*k3', '-s/2-t/2'),
+                                ('k3*k2', 't/2'),
+                            ],
+
+            dimensionality = dim,
+
+            powerlist = powerlist
+
+        ),
+
+        real_parameters = real_parameters,
+
+        # the optimization level to use in FORM (can be 0, 1, 2, 3, 4)
+        form_optimization_level = 2,
+
+        # the WorkSpace parameter for FORM
+        form_work_space = '100M',
+
+        # the method to be used for the sector decomposition
+        # valid values are ``iterative`` or ``geometric`` or ``geometric_ku``
+        decomposition_method = 'iterative',
+        # if you choose ``geometric[_ku]`` and 'normaliz' is not in your
+        # $PATH, you can set the path to the 'normaliz' command-line
+        # executable here
+        #normaliz_executable='/path/to/normaliz',
+
+    )
+
+integrals = [
+                make_1L_integral([0,1,1,1]),
+                make_1L_integral([1,0,1,1]),
+                make_1L_integral([1,1,0,1]),
+                make_1L_integral([1,1,1,0]),
+                make_1L_integral([1,1,1,1], dim='6-2*eps'),
+            ]
+
+coefficients = [
+                    Coefficient(['1'], ['s'], ['eps'], real_parameters),
+                    Coefficient(['1'], ['t'], ['eps'], real_parameters),
+                    Coefficient(['1'], ['s'], ['eps'], real_parameters),
+                    Coefficient(['1'], ['t'], ['eps'], real_parameters),
+                    Coefficient(['2*(s+t)'], ['s*t'], ['eps'], real_parameters),
+               ]
+
+# reduced
+sum_package(
+
+    name = 'D0_reduced',
+
+    coefficients = coefficients,
+    package_generators = [loop_package]*len(integrals),
+    generators_args = integrals,
+
+    regulators = ['eps'],
+    requested_orders = [0],
+
+    real_parameters = real_parameters
+
+)
+
+# direct
+args = make_1L_integral([1,1,1,1])
+args['name'] = 'D0_direct'
+args['requested_order'] = 0
+loop_package(**args)
+
+
+
+
