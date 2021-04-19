0b315e17707c7146391fb94043632712ce76457d
Vitaly Magerya <magv@tx97.net>
2021-04-19 19:02:04 +0200

    [scons] make the C++ interface work too

---
+++ pySecDecContrib/__main__.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    import sys
+    from . import dirname
+    argv = sys.argv[1:]
+    if argv == ["--dirname"]:
+        print(dirname)
+    else:
+        print("Usage: python3 -m pySecDecContrib --dirname\n", file=sys.stderr)
+        exit(1)
