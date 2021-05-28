85071af157784c2456b05c03c3b6251eba11224a
Villa Emilio <vemilio@itpgpu1.itp.kit.edu>
2021-05-28 15:19:08 +0200

    [examples] Update 4yyyy amplitude with sum_package

--- nodist_examples/4photon1L_amplitude_sum_package/coefficients.py
+++ nodist_examples/4photon1L_amplitude_sum_package/coefficients.py
@@ -3,12 +3,17 @@ from pySecDec.code_writer.sum_package import Coefficient
 ### Coefficients definitions ###
 
 coeff = [ 
+    
     # bubble (u) coefficient
-    [Coefficient(['t-u'],['-u-t'],['eps'],['t','u']),
+    [Coefficient(['-8*(t-u)'],['-u-t'],['eps'],['t','u']),
     
     # bubble (t) coefficient
-    Coefficient(['u-t'],['-u-t'],['eps'],['t','u']),
+    Coefficient(['-8*(u-t)'],['-u-t'],['eps'],['t','u']),
 
-    # box coefficient
-    Coefficient(['t**2+u**2'],['-u-t'],['eps'],['t','u'])]
+    # box6 coefficient
+    Coefficient(['-8*(t**2+u**2)'],['-u-t'],['eps'],['t','u']),
+    
+    # box8 coefficient
+    Coefficient(['-8*3*(2*eps)'],['1'],['eps'],['t','u'])]
+
 ]
--- nodist_examples/4photon1L_amplitude_sum_package/generate.py
+++ nodist_examples/4photon1L_amplitude_sum_package/generate.py
@@ -9,33 +9,19 @@ from pySecDec.code_writer.sum_package import Coefficient
 import integrals
 import coefficients 
 
-# define input to loop_package for one loop bubble (u)
-args_bu = {
-    'name' : 'bu',
-    'loop_integral' : integrals.li_bu,
-    'requested_orders' : [0]
-    }
-
-# define input to loop_package for one loop bubble (t)
-args_bt = {
-    'name' : 'bt',
-    'loop_integral' : integrals.li_bt,
-    'requested_orders' : [0]
-    }
-
-# define input to loop_package for one loop box
-args_box = {
-    'name' : 'box',
-    'loop_integral' : integrals.li_box,
-    'requested_orders' : [0]
-    }
-
-# collect args_integral in args as list
-args = [args_bu, args_bt, args_box]
+# define input to loop_package
+args = []
+names = ['bubble_u', 'bubble_t', 'box_6', 'box_8']
+for i in range(len(integrals.I)):
+    args.append({
+        'name'              : names[i],
+        'loop_integral'     : integrals.I[i],
+        'requested_orders'  : [0]
+        })
 
 # generate code that will calculate the sum of all integrals times the corresponding coefficients 
 psd.code_writer.sum_package('yyyy1L_amp',
-    [psd.loop_integral.loop_package]*len(args),
+    [psd.loop_integral.loop_package]*len(args), 
     args, regulators = ['eps'], requested_orders = [0],
     real_parameters = ['t','u'],
     coefficients = coefficients.coeff,
