fa5ec782e6f4a8ea328364e92b295d241493f01e
Stephen Jones <s.jones@cern.ch>
2021-07-05 14:41:22 +0100

    [high_level_tests] add box1L_rank4_make_package (checks old behaviour)

---
+++ high_level_tests/box1L_rank4_make_package/box1L_rank4.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+from pySecDec.loop_integral import loop_package
+from pySecDec.code_writer.make_package import make_package as code_writer_make_package
+import pySecDec as psd
+
+if __name__ == "__main__":
+
+    li = psd.loop_integral.LoopIntegralFromPropagators(
+    loop_momenta = ['k1'],
+    external_momenta = ['p1','p2','p3','p4'],
+    Lorentz_indices = ['mu1','mu2','mu3','mu4'],
+
+    propagators = ['k1**2-msq','(k1+p1)**2-msq','(k1+p1+p2)**2-msq','(k1+p1+p2+p3)**2-msq'],
+    powerlist = [1,1,1,1],
+    numerator = 'k1(mu1)*p1(mu1)*k1(mu2)*p2(mu2)*k1(mu3)*p3(mu3)*k1(mu4)*p4(mu4)',
+
+    replacement_rules = [
+                            ('p1*p1', 0),
+                            ('p2*p2', 0),
+                            ('p3*p3', 0),
+                            ('p4*p4', 0),
+                            ('p3*p1', '-t/2-s/2'),
+                            ('p3*p2', 't/2'),
+                            ('p1*p2', 's/2'),
+                            ('p1*p4', 't/2'),
+                            ('p2*p4', '-t/2-s/2'),
+                            ('p3*p4', 's/2'),
+                            ('m**2', 'msq')
+                        ]
+    )
+
+    Mandelstam_symbols = ['s','t']
+    mass_symbols = ['msq']
+
+    loop_package(
+
+    name = 'box1L_rank4',
+    #name = 'test',
+
+    loop_integral = li,
+
+    real_parameters = Mandelstam_symbols + mass_symbols,
+
+    # the highest order of the final epsilon expansion --> change this value to whatever you think is appropriate
+    requested_orders = [0],
+
+    # the optimization level to use in FORM (can be 0, 1, 2, 3, 4)
+    form_optimization_level = 3,
+
+    # the WorkSpace parameter for FORM
+    form_work_space = '100M',
+
+    # the method to be used for the sector decomposition
+    # valid values are ``iterative`` or ``geometric`` or ``geometric_ku``
+    decomposition_method = 'iterative',
+    # if you choose ``geometric[_ku]`` and 'normaliz' is not in your
+    # $PATH, you can set the path to the 'normaliz' command-line
+    # executable here
+    #normaliz_executable='/path/to/normaliz',
+    contour_deformation = True,
+    
+    package_generator = code_writer_make_package
+    
+
+    )
---
+++ high_level_tests/box1L_rank4_make_package/test/test.py
@@ -0,0 +1,75 @@
+from __future__ import print_function
+from pySecDec.integral_interface import IntegralLibrary
+import sympy as sp
+import unittest
+
+class CheckLib(unittest.TestCase):
+    def setUp(self):
+        # load c++ library
+        self.lib = IntegralLibrary('../box1L_rank4/box1L_rank4_pylink.so')
+        self.real_parameters = [16.0, -75.0, 1.0]
+        self.maxeval = 10**8
+        self.epsrel = 1e-10
+        self.epsabs = 1e-13
+
+        self.target_result_without_prefactor = \
+        {
+               0:   97.52083333333 +  0.0j,
+               1: -124.937368867   - 11.9030583278j
+        }
+        self.target_prefactor = \
+        {
+              -1:  1.0,
+               0: -0.57721566490153,
+        }
+        self.target_result_with_prefactor = \
+        {
+              -1:   97.52083333333 +  0.0j,
+               0: -181.22792152123 - 11.903058327787j
+        }
+
+    def check_result(self, computed_series, target_series, epsrel, epsabs, order_min, order_max):
+        # convert result to sympy expressions
+        computed_series = sp.sympify(  computed_series.replace(',','+I*').replace('+/-','*value+error*')  )
+
+        for order in range(order_min, order_max+1):
+            value = complex( computed_series.coeff('eps',order).coeff('value') )
+            error = complex( computed_series.coeff('eps',order).coeff('error') )
+
+            # check that the uncertainties are reasonable
+            self.assertLessEqual(error.real, abs(2*epsrel * target_series[order].real))
+            if target_series[order].imag != 0.0:
+                self.assertLessEqual(error.imag, abs(2*epsrel * target_series[order].imag))
+
+            # check that the desired uncertainties are reached
+            self.assertLessEqual(error.real, abs(epsrel * value.real) )
+            if target_series[order].imag == 0.0:
+                self.assertLessEqual(error.imag, epsabs)
+            else:
+                self.assertLessEqual(error.imag, abs(epsrel * value.imag) )
+
+            # check integral value
+            self.assertAlmostEqual(  value.real, target_series[order].real, delta=epsrel*abs(target_series[order].real)  )
+            if target_series[order].imag == 0.0:
+                self.assertAlmostEqual(  value.imag, target_series[order].imag, delta=epsabs  )
+            else:
+                self.assertAlmostEqual(  value.imag, target_series[order].imag, delta=epsrel*abs(target_series[order].imag)  )
+
+    def test_Cuhre(self):
+        # choose integrator
+        self.lib.use_Cuhre(epsrel=self.epsrel, maxeval=self.maxeval, epsabs=self.epsabs, real_complex_together=True)
+
+        # integrate
+        str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = self.lib(self.real_parameters)
+
+        # check integral
+        self.check_result(str_integral_without_prefactor, self.target_result_without_prefactor, self.epsrel, self.epsabs, order_min=0, order_max=1)
+        self.check_result(str_integral_with_prefactor, self.target_result_with_prefactor, self.epsrel, self.epsabs, order_min=-1, order_max=0)
+
+        # check prefactor
+        prefactor = sp.sympify(  str_prefactor.replace(',','+I*')  )
+        for order in (-1,0):
+            self.assertAlmostEqual(  prefactor.coeff('eps', order), self.target_prefactor[order], delta=1e-13  )
+
+if __name__ == '__main__':
+    unittest.main()
