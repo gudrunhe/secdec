0b91097293d5493a0aba7d4cabef5eb44f8a5410
Villa Emilio <emilio.villa@kit.edu>
2021-07-08 10:16:59 +0200

    [examples] Update box1L_expansion_by_regions

--- nodist_examples/box1L_expansion_by_regions/generate_box1L_expansion_by_regions.py
+++ nodist_examples/box1L_expansion_by_regions/generate_box1L_expansion_by_regions.py
@@ -2,88 +2,54 @@
 
 from pySecDec import sum_package
 from pySecDec.loop_integral import loop_regions
-import numpy as np
 import pySecDec as psd
 
-# This example is the first one loop box example in the Go Mishima paper arXiv:1812.04373
+# This example is the first one loop box example in Go Mishima paper arXiv:1812.04373
 
-# here we define the Feynman diagram
-li = psd.loop_integral.LoopIntegralFromGraph(
-internal_lines = [['mt',[3,1]],['mt',[1,2]],['mt',[2,4]],['mt',[4,3]]],
-external_lines = [['p1',1],['p2',2],['p3',3],['p4',4]],
-powerlist=["1+n1","1+n1/2","1+n1/3","1+n1/5"],
-regulators=["eps","n1"],
-Feynman_parameters=["x%i" % i for i in range(1,5)], # this renames the parameters, so we get the same polynomials as in the paper
+if __name__ == "__main__":
 
-replacement_rules = [
-                        # note that in those relations all momenta are incoming
-                        # below we have general relations
-                        ('p1*p1', 'm1sq'),
-                        ('p2*p2', 'm2sq'),
-                        ('p3*p3', 'm3sq'),
-                        ('p4*p4', 'm4sq'),
-                        ('p1*p2', 's/2-(m1sq+m2sq)/2'),
-                        ('p1*p3', 't/2-(m1sq+m3sq)/2'),
-                        ('p1*p4', 'u/2-(m1sq+m4sq)/2'),
-                        ('p2*p3', 'u/2-(m2sq+m3sq)/2'),
-                        ('p2*p4', 't/2-(m2sq+m4sq)/2'),
-                        ('p3*p4', 's/2-(m3sq+m4sq)/2'),
-                        ('u', '(m1sq+m2sq+m3sq+m4sq)-s-t'),
-                        # these below are for our specific case
-                        ('mt**2', 'mtsq'),
-                        ('m1sq',0),
-                        ('m2sq',0),
-                        ('m3sq','mHsq'),
-                        ('m4sq','mHsq'),
-                        ('mHsq', 0),
-                    ]
-)
+    # here we define the Feynman diagram
+    li = psd.loop_integral.LoopIntegralFromGraph(
+    internal_lines = [['mt',[3,1]],['mt',[1,2]],['mt',[2,4]],['mt',[4,3]]],
+    external_lines = [['p1',1],['p2',2],['p3',3],['p4',4]],
+    powerlist=["1+n1","1+n1/2","1+n1/3","1+n1/5"],
+    regulators=["eps","n1"],
+    Feynman_parameters=["x%i" % i for i in range(1,5)], # this renames the parameters, so we get the same polynomials as in the paper
 
-def f(s,t,u, mtsq):
-    """
-    The resulting function of the integral from arXiv:1812.04373, to order 0 in mtsq.
+    replacement_rules = [
+                            # note that in those relations all momenta are incoming
+                            # below we have general relations
+                            ('p1*p1', 'm1sq'),
+                            ('p2*p2', 'm2sq'),
+                            ('p3*p3', 'm3sq'),
+                            ('p4*p4', 'm4sq'),
+                            ('p1*p2', 's/2-(m1sq+m2sq)/2'),
+                            ('p1*p3', 't/2-(m1sq+m3sq)/2'),
+                            ('p1*p4', 'u/2-(m1sq+m4sq)/2'),
+                            ('p2*p3', 'u/2-(m2sq+m3sq)/2'),
+                            ('p2*p4', 't/2-(m2sq+m4sq)/2'),
+                            ('p3*p4', 's/2-(m3sq+m4sq)/2'),
+                            ('u', '(m1sq+m2sq+m3sq+m4sq)-s-t'),
+                            # these below are for our specific case
+                            ('mt**2', 'mtsq'),
+                            ('m1sq',0),
+                            ('m2sq',0),
+                            ('m3sq','mHsq'),
+                            ('m4sq','mHsq'),
+                            ('mHsq', 0),
+                        ])
 
-    Expected Result from the paper for ['s','t','u','mtsq'] = [4.0, -2.82842712475, -2.82842712475, 0.1]:
-    (-1.30718609739+1.85618421207j)
-    """
-    v = -t/s
-    lsm = np.log(complex(s/mtsq))
-    return 1/(s**2*v)*(np.pi**2-2*(lsm-1j*np.pi)*(lsm+np.log(complex(v))))
+    # find the regions
+    generators_args = loop_regions(
+        name = "box1L_expansion_by_regions",
+        loop_integral=li,
+        smallness_parameter = "mtsq",
+        expansion_by_regions_order=0)
 
-def fourvec(m,p):
-    # creates a four-vector from the mass and the momentum three-vector
-    return np.array([np.linalg.norm([m]+p)]+p)
-
-def get_stu(p1,p2,p3):
-    def fourproduct(v1,v2):
-        return v1[0]*v2[0]-np.dot(v1[1:],v2[1:])
-    _s = p1+p2
-    _t = p2-p3
-    _u = p1-p3
-    return fourproduct(_s,_s), fourproduct(_t,_t), fourproduct(_u,_u)
-
-# here we define the fourvectors and then calculate s,t,u from them,
-# to get an example numeric value with which to compare the result of the
-# integration
-p1 = fourvec(0,[1,0,0])
-p2 = fourvec(0,[-1,0,0])
-p3 = fourvec(0,[0,1,1])
-
-s,t,u = get_stu(p1,p2,p3)
-mtsq=0.1
-
-print("When using s, t, u, mtsq = {:.5f}, {:.5f}, {:.5f}, {:.5f}".format(s,t,u,mtsq))
-print("The expected result is: {:.5f}".format(f(s,t,u,mtsq)))
-print()
-
-# find the regions
-generators_args = loop_regions(
-    name = "box1L_expansion_by_regions",
-    loop_integral=li,
-    smallness_parameter = "mtsq",
-    expansion_by_regions_order=0)
-
-# write the code to sum up the regions
-sum_package("box1L_expansion_by_regions", generators_args, li.regulators,
-            requested_orders = [0,0],
-            real_parameters = ['s','t','u','mtsq'], complex_parameters = [])
+    # write the code to sum up the regions
+    sum_package("box1L_expansion_by_regions",
+                generators_args,
+                li.regulators,
+                requested_orders = [0,0],
+                real_parameters = ['s','t','u','mtsq'],
+                complex_parameters = [])
--- nodist_examples/box1L_expansion_by_regions/integrate_box1L_expansion_by_regions.py
+++ nodist_examples/box1L_expansion_by_regions/integrate_box1L_expansion_by_regions.py
@@ -2,11 +2,37 @@
 
 from pySecDec.integral_interface import IntegralLibrary
 import sympy as sp
+import numpy as np
+
+# define analytic result
+def f(s,t,u, mtsq):
+    """
+    The resulting function of the integral from arXiv:1812.04373, to order 0 in mtsq.
+
+    Expected Result from the paper for ['s','t','u','mtsq'] = [4.0, -2.82842712475, -2.82842712475, 0.1]:
+    (-1.30718609739+1.85618421207j)
+    """
+    v = -t/s
+    lsm = np.log(complex(s/mtsq))
+    return 1/(s**2*v)*(np.pi**2-2*(lsm-1j*np.pi)*(lsm+np.log(complex(v))))
+
+# define useful functions
+def fourvec(m,p):
+    # creates a four-vector from the mass and the momentum three-vector
+    return np.array([np.linalg.norm([m]+p)]+p)
+
+def get_stu(p1,p2,p3):
+    def fourproduct(v1,v2):
+        return v1[0]*v2[0]-np.dot(v1[1:],v2[1:])
+    _s = p1+p2
+    _t = p2-p3
+    _u = p1-p3
+    return fourproduct(_s,_s), fourproduct(_t,_t), fourproduct(_u,_u)
 
 # load c++ library
 name = "box1L_expansion_by_regions"
 intlib = IntegralLibrary("{0}/{0}_pylink.so".format(name))
-intlib.use_Qmc(transform="korobov3", fitfunction="polysingular")
+intlib.use_Qmc(transform="korobov3", fitfunction="polysingular", verbosity=1)
 
 # integrate
 str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = intlib(real_parameters=[4.0, -2.82842712475, -2.82842712475, 0.1])
@@ -18,9 +44,20 @@ str_integral_with_prefactor = str_integral_with_prefactor.replace(',','+I*')
 integral_result = sp.sympify(str_integral_with_prefactor.replace('+/-','*value+error*'))
 integral_result_err = sp.sympify(str_integral_with_prefactor.replace('+/-','*value+error*'))
 
+# print analytic result for given kinematics
+p1 = fourvec(0,[1,0,0])
+p2 = fourvec(0,[-1,0,0])
+p3 = fourvec(0,[0,1,1])
+s,t,u = get_stu(p1,p2,p3)
+mtsq=0.1
+
+print("When using s, t, u, mtsq = {:.5f}, {:.5f}, {:.5f}, {:.5f}".format(s,t,u,mtsq))
+print("The expected result is: {:.15f}".format(f(s,t,u,mtsq)))
+print()
+
 # examples how to access individual orders
 print('Numerical Result')
 for power in [0]:
     valreal, valimg = integral_result.coeff('eps',power).coeff('value').as_real_imag()
     errreal, errimg = integral_result.coeff('eps',power).coeff('error').as_real_imag()
-    print("eps^{:<2} {: .5f}{:+.5f}*I +/- {: .5f}{:+.5f}*I".format(power,float(valreal),float(valimg),float(errreal),float(errimg)))
+    print("eps^{:<2} {: .15f}{:+.15f}*I +/- {:.15f}{:+.15f}*I".format(power,float(valreal),float(valimg),float(errreal),float(errimg)))
