f3fbcb5a9cafb01ed135e1466e725aa0fca01dc0
Villa Emilio <emilio.villa@kit.edu>
2021-07-08 17:29:04 +0200

    [examples] update formfactor1L_massive_expansion_by_regions

--- nodist_examples/formfactor1L_expansion_by_regions/generate_formfactor1L_massive_expansion_by_regions.py
+++ nodist_examples/formfactor1L_expansion_by_regions/generate_formfactor1L_massive_expansion_by_regions.py
@@ -6,20 +6,17 @@ import pySecDec as psd
 
 if __name__ == '__main__':
 
-    # Expected Result from Eq(6.30) of arXiv:1111.2589
-    # N[-1/qsq*(1/2*Log[qsq/msq]^2 + Log[qsq/msq]*Log[1 - msq/qsq] - PolyLog[2, msq/qsq] + Pi^2/3) /. {qsq -> 100, msq -> 1}, 20]
-    # -0.13837355735881397785 + O(eps) + O(n1)
-
     # Example 6 of Bernd Jantzen (arXiv:1111.2589)
+
     regions_generators = make_regions(
 
     # make_regions_args
     name = 'formfactor1L_massive_expansion_by_regions',
     integration_variables = ['x0','x1','x2'],
-    regulators = ['n0','n1','eps'],
-    requested_orders = [0,0,0],
+    regulators = ['n','eps'],
+    requested_orders = [0,0],
     smallness_parameter = 'z',
-    polynomials_to_decompose = ['(x0+x1+x2)**((1+n0)+(1+n1)-3+2*eps)','((z**2*msq)*x2*(x0+x1+x2)+(qsq)*x0*x1)**(-(1+n0)-(1+n1)+1-eps)','x0**n0','x1**n1'],
+    polynomials_to_decompose = ['(x0+x1+x2)**((1+n/2)+(1+n/3)-3+2*eps)','((z*msq)*x2*(x0+x1+x2)+(qsq)*x0*x1)**(-(1+n/2)-(1+n/3)+1-eps)','x0**(n/2)','x1**(n/3)'],
     expansion_by_regions_order = 0,
     real_parameters = ['qsq','msq','z'],
     complex_parameters = [],
@@ -28,12 +25,12 @@ if __name__ == '__main__':
     polynomial_names = ['U','F'],
     contour_deformation_polynomial = 'F',
     positive_polynomials = ['U'],
-    prefactor = '-gamma(eps + 1)', # limits n0,n1->0 already taken in the prefactor
+    prefactor = '-gamma(eps + 1)', # limits n->0 already taken in prefactor
     decomposition_method = 'geometric',
-    polytope_from_sum_of=[0,1]
-    )
+    polytope_from_sum_of=[0,1])
 
+    # sum_package
     sum_package('formfactor1L_massive_expansion_by_regions',
-        regions_generators, regulators = ['n0','n1','eps'],requested_orders = [0,0,0],
+        regions_generators, regulators = ['n','eps'], requested_orders = [0,0],
         real_parameters = ['qsq','msq','z'],
         complex_parameters = [])
--- nodist_examples/formfactor1L_expansion_by_regions/integrate_formfactor1L_massive_expansion_by_regions.py
+++ nodist_examples/formfactor1L_expansion_by_regions/integrate_formfactor1L_massive_expansion_by_regions.py
@@ -6,7 +6,7 @@ import sympy as sp
 # load c++ library
 name = "formfactor1L_massive_expansion_by_regions"
 intlib = IntegralLibrary(f"{name}/{name}_pylink.so")
-intlib.use_Qmc(transform="korobov3", fitfunction="polysingular")
+intlib.use_Qmc(transform="korobov3", fitfunction="polysingular", verbosity=1)
 
 # integrate
 str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = intlib(real_parameters=[100,1,1])
@@ -23,4 +23,10 @@ print('Numerical Result')
 for power in [0]:
     valreal, valimg = integral_result.coeff('eps',power).coeff('value').as_real_imag()
     errreal, errimg = integral_result.coeff('eps',power).coeff('error').as_real_imag()
-    print("eps^{:<2} {: .5f}{:+.5f}*I +/- {: .5e}{:+.5e}*I".format(power,float(valreal),float(valimg),float(errreal),float(errimg)))
+    print("eps^{:<2} {: .15f}{:+.15f}*I +/- {:.15e}{:+.15e}*I".format(power,float(valreal),float(valimg),float(errreal),float(errimg)))
+
+# analytic result
+# from Eq(6.30) of arXiv:1111.2589
+# N[-1/qsq*(1/2*Log[qsq/msq]^2 + Log[qsq/msq]*Log[1 - msq/qsq] - PolyLog[2, msq/qsq] + Pi^2/3) /. {qsq -> 100, msq -> 1}, 20]
+print('\nAnalytic Result for qsq = 100, msq = 1')
+print("eps^0  -0.13837355735881397785+0*I")
