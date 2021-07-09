be00da05c455889c0157c3cedb959a2b26f4a81d
Stephen Jones <s.jones@cern.ch>
2021-07-09 12:24:40 +0100

    [high_level_tests] copy old one_integration_variable with prefix make_package

---
+++ high_level_tests/make_package_one_integration_variable/generate_one_integration_variable.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import shutil
+from pySecDec.code_writer import make_package
+
+if __name__ == "__main__":
+
+    name = 'one_integration_variable'
+
+    make_package(
+
+    name=name,
+    integration_variables = ['x'],
+
+    # the order here defines the order of the expansion
+    regulators = ['eps'],
+
+    # the highest orders of the final regulator expansion
+    # the order here matches the order of ``regulators``
+    requested_orders = [1],
+
+    polynomials_to_decompose = ['(x)**(-1+eps)'],
+    polynomial_names = ['p'],
+    other_polynomials = ['p**-2'],
+    pylink_qmc_transforms=['korobov3', 'korobov2x1', 'sidi3']
+    )
---
+++ high_level_tests/make_package_one_integration_variable/test/test.py
@@ -0,0 +1,130 @@
+from __future__ import print_function
+from pySecDec.integral_interface import IntegralLibrary
+import sympy as sp
+import unittest
+
+class CheckQmcErrorMessages(unittest.TestCase):
+    def setUp(self):
+        # load c++ library
+        self.lib = IntegralLibrary('../one_integration_variable/one_integration_variable_pylink.so')
+
+    def test_setting_errormode(self):
+        self.assertRaisesRegexp(ValueError, 'Unknown `errormode` "foo"', self.lib.use_Qmc, transform='korobov3', errormode='foo')
+
+        # test known errormodes
+        self.lib.use_Qmc(errormode='default', transform='korobov3')
+        self.lib.use_Qmc(errormode='all', transform='korobov3')
+        self.lib.use_Qmc(errormode='largest', transform='korobov3')
+
+class CheckLib(unittest.TestCase):
+    def setUp(self):
+        # load c++ library
+        self.lib = IntegralLibrary('../one_integration_variable/one_integration_variable_pylink.so')
+        self.target_result = {
+                                 -1:  0.0,
+                                  0: -0.5,
+                                  1: -0.25
+                             }
+        self.epsrel = 1e-11
+        self.epsabs = 1e-10
+        self.maxeval = 10**5
+
+    def check_result(self, computed_series, epsrel, epsabs):
+        # convert result to sympy expressions
+        integral_with_prefactor = sp.sympify(  computed_series.replace(',','+I*').replace('+/-','*value+error*')  )
+
+        for order in range(-1,2):
+            value = complex( integral_with_prefactor.coeff('eps',order).coeff('value') )
+            error = complex( integral_with_prefactor.coeff('eps',order).coeff('error') )
+
+            # check that the desired uncertainties are reached
+            self.assertLessEqual(error.real, epsabs)
+            self.assertLessEqual(error.imag, epsabs)
+
+            # check integral value
+            self.assertAlmostEqual(  value.real, self.target_result[order].real, delta=epsabs  )
+            self.assertAlmostEqual(  value.imag, self.target_result[order].imag, delta=epsabs  )
+
+    def test_Vegas(self):
+        # choose integrator
+        # can only reach ~2e-9 accuracy with Vegas
+        self.lib.use_Vegas(flags=0, epsrel=self.epsrel, epsabs=2e-9, maxeval=self.maxeval) # ``flags=2``: verbose --> see Cuba manual
+
+        # integrate
+        str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = self.lib()
+
+        # check
+        self.check_result(str_integral_with_prefactor, self.epsrel, 2e-9)
+
+    def test_Suave(self):
+        # choose integrator
+        self.lib.use_Suave(flags=0, epsrel=self.epsrel, epsabs=self.epsabs, maxeval=self.maxeval) # ``flags=2``: verbose --> see Cuba manual
+
+        # integrate
+        str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = self.lib()
+
+        # check
+        self.check_result(str_integral_with_prefactor, self.epsrel, self.epsabs)
+
+    def test_Divonne(self):
+        # choose integrator
+        self.lib.use_Divonne(flags=0, epsrel=self.epsrel, epsabs=self.epsabs, border=1e-8, maxeval=self.maxeval) # ``flags=2``: verbose --> see Cuba manual
+
+        # integrate
+        str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = self.lib()
+
+        # check
+        self.check_result(str_integral_with_prefactor, self.epsrel, self.epsabs)
+
+    def test_Cuhre(self):
+        # choose integrator
+        self.lib.use_Cuhre(flags=0, epsrel=self.epsrel, epsabs=self.epsabs, maxeval=self.maxeval) # ``flags=2``: verbose --> see Cuba manual
+
+        # integrate
+        str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = self.lib()
+
+        # check
+        self.check_result(str_integral_with_prefactor, self.epsrel, self.epsabs)
+
+    def test_CQuad(self):
+        # choose integrator
+        self.lib.use_CQuad(verbose=False, epsrel=self.epsrel, epsabs=self.epsabs)
+
+        # integrate
+        str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = self.lib()
+
+        # check
+        self.check_result(str_integral_with_prefactor, self.epsrel, self.epsabs)
+
+    def test_Qmc_default_transform(self):
+        # choose integrator
+        self.lib.use_Qmc(verbosity=0, epsrel=self.epsrel, epsabs=self.epsabs, seed=3212, transform='korobov3', fitfunction='polysingular')
+
+        # integrate
+        str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = self.lib()
+
+        # check
+        self.check_result(str_integral_with_prefactor, self.epsrel, self.epsabs)
+
+    def test_Qmc_no_transform(self):
+        # choose integrator
+        self.lib.use_Qmc(verbosity=0, epsrel=self.epsrel, epsabs=self.epsabs, seed=3212, transform='none', fitfunction='none')
+
+        # integrate
+        str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = self.lib()
+
+        # check
+        self.check_result(str_integral_with_prefactor, self.epsrel, self.epsabs)
+
+    def test_Qmc_Korobov2x1_transform(self):
+        # choose integrator
+        self.lib.use_Qmc(verbosity=0, epsrel=self.epsrel, epsabs=self.epsabs, seed=3212, transform='Korobov2x1')
+
+        # integrate
+        str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = self.lib()
+
+        # check
+        self.check_result(str_integral_with_prefactor, self.epsrel, self.epsabs)
+
+if __name__ == '__main__':
+    unittest.main()
