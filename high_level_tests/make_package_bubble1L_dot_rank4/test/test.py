47b0a5f1f33591e6f7504a82463664bbeb05919f
Stephen Jones <s.jones@cern.ch>
2021-07-09 01:53:25 +0100

    [high_level_tests] copy old bubble1L_dot_rank4 with prefix make_package

---
+++ high_level_tests/make_package_bubble1L_dot_rank4/test/test.py
@@ -0,0 +1,95 @@
+from __future__ import print_function
+from pySecDec.integral_interface import IntegralLibrary
+import sympy as sp
+import unittest
+
+class CheckLib(unittest.TestCase):
+    def setUp(self):
+        # load c++ library
+        self.lib = IntegralLibrary('../bubble1L_dot_rank4/bubble1L_dot_rank4_pylink.so')
+
+        # set global options
+        self.real_parameters = [1.275, 1.275]
+        self.complex_parameters = [30.886875, 30.886875, 123.5475]
+        self.maxeval = 10**6
+        self.epsrel = 1e-4
+        self.epsabs = 1e-7
+
+        self.target_result_with_prefactor = \
+        {
+              -1:  1.0    + 0.0j,
+               0: -1.2708 + 2.4179j
+        }
+        self.order_min = -1
+        self.order_max =  0
+
+    def check_result(self, computed_series, target_series, epsrel, epsabs):
+        # convert result to sympy expressions
+        computed_series = sp.sympify(  computed_series.replace(',','+I*').replace('+/-','*value+error*')  )
+
+        for order in range(self.order_min, self.order_max+1):
+            value = complex( computed_series.coeff('eps',order).coeff('value') )
+            error = complex( computed_series.coeff('eps',order).coeff('error') )
+
+            # check that the uncertainties are reasonable
+            self.assertLessEqual(error.real, abs(2*epsrel * target_series[order].real))
+            if target_series[order].imag != 0.0:
+                self.assertLessEqual(error.imag, abs(2*epsrel * target_series[order].imag))
+
+            # check that the desired uncertainties are reached
+            self.assertLessEqual(error.real, abs(epsrel * value.real) )
+            if target_series[order].imag == 0.0:
+                self.assertLessEqual(error.imag, epsabs)
+            else:
+                self.assertLessEqual(error.imag, abs(epsrel * value.imag) )
+
+            # check integral value
+            self.assertAlmostEqual(  value.real, target_series[order].real, delta=epsrel*abs(target_series[order].real)  )
+            if target_series[order].imag == 0.0:
+                self.assertAlmostEqual(  value.imag, target_series[order].imag, delta=epsabs  )
+            else:
+                self.assertAlmostEqual(  value.imag, target_series[order].imag, delta=epsrel*abs(target_series[order].imag)  )
+
+    def test_Cuhre(self):
+        # choose integrator
+        self.lib.use_Cuhre(epsrel=self.epsrel, maxeval=self.maxeval, epsabs=self.epsabs, real_complex_together=True, flags=0)
+
+        # integrate
+        str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = self.lib(self.real_parameters, self.complex_parameters)
+
+        # check integral
+        self.check_result(str_integral_with_prefactor, self.target_result_with_prefactor, self.epsrel, self.epsabs)
+
+    def test_Cuhre_CQuad(self):
+        # choose integrator
+        self.lib.use_Cuhre(epsrel=self.epsrel, maxeval=self.maxeval, epsabs=self.epsabs, real_complex_together=True, flags=0)
+        self.lib.use_CQuad(epsrel=self.epsrel, epsabs=self.epsabs, verbose=False)
+
+        # integrate
+        str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = self.lib(self.real_parameters, self.complex_parameters)
+
+        # check integral
+        self.check_result(str_integral_with_prefactor, self.target_result_with_prefactor, self.epsrel, self.epsabs)
+
+    def test_Qmc_default_integral_transform(self):
+        # choose integrator
+        self.lib.use_Qmc(epsrel=self.epsrel, maxeval=self.maxeval, epsabs=self.epsabs, verbosity=0, seed=143, transform='korobov3')
+
+        # integrate
+        str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = self.lib(self.real_parameters, self.complex_parameters)
+
+        # check integral
+        self.check_result(str_integral_with_prefactor, self.target_result_with_prefactor, self.epsrel, self.epsabs)
+
+    def test_Qmc_baker_transform(self):
+        # choose integrator
+        self.lib.use_Qmc(epsrel=self.epsrel, maxeval=self.maxeval, epsabs=self.epsabs, verbosity=0, seed=143, transform='baker', evaluateminn=0, fitfunction='polysingular')
+
+        # integrate
+        str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = self.lib(self.real_parameters, self.complex_parameters)
+
+        # check integral
+        self.check_result(str_integral_with_prefactor, self.target_result_with_prefactor, self.epsrel, self.epsabs)
+
+if __name__ == '__main__':
+    unittest.main()
