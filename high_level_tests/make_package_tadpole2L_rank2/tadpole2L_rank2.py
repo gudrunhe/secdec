a4ab85ae0e56a3696512c2db94b6562124e3e704
Stephen Jones <s.jones@cern.ch>
2021-07-09 13:18:31 +0100

    [high_level_tests] copy old tadpole2L_rank2  with prefix make_package

---
+++ high_level_tests/make_package_tadpole2L_rank2/tadpole2L_rank2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+from pySecDec.loop_integral import loop_package
+import pySecDec as psd
+
+if __name__ == "__main__":
+
+    li = psd.loop_integral.LoopIntegralFromPropagators(
+    loop_momenta = ['k1','k2'],
+    Lorentz_indices = ['mu'],
+    propagators = ['k1^2 - m1sq', 'k2^2 - m2sq', '(k1+k2)^2 - m3sq'],
+    numerator = '2*k1(mu)*k2(mu)',
+    )
+
+
+    loop_package(
+
+    name = 'tadpole2L_rank2',
+
+    loop_integral = li,
+
+    # the highest order of the final epsilon expansion --> change this value to whatever you think is appropriate
+    requested_orders = [0],
+
+    real_parameters =['m1sq','m2sq','m3sq'],
+
+    # the optimization level to use in FORM (can be 0, 1, 2, 3, 4)
+    form_optimization_level = 3,
+
+    # contour deformation is not required because there are no external legs
+    contour_deformation = False,
+
+    # make sure the retun type is double
+    enforce_complex = True,
+
+    package_generator=psd.code_writer.make_package
+    )
