e3f615aa319af6f80a54cca8ad48136cc335db2e
Stephen Jones <s.jones@cern.ch>
2021-07-12 12:37:33 +0100

    [high_level_tests] copy old selective_ibp with prefix make_package

---
+++ high_level_tests/make_package_selective_ibp/functions_template.hpp
@@ -0,0 +1,44 @@
+#ifndef userdefined_cpp_src_functions_hpp_included
+#define userdefined_cpp_src_functions_hpp_included
+
+#include "userdefined_cpp.hpp"
+#include "SecDecInternalFunctions.hpp"
+
+#include <cmath>
+#include <complex>
+
+namespace userdefined_cpp
+{
+
+    /*
+     * Declarations of the `functions` and their required
+     * derivatives are declared here. The derivative of a function
+     * 'f' by its i-th argument is denoted as 'dfdi'. To implement
+     * the functions listed below, you can either add "inline"
+     * keywords and define these functions here, or you define the
+     * functions in a separate '.cpp' file. If you decide for a
+     * separate file, the file name can be arbitrary up to the
+     * '.cpp' suffix. Furthermore, the '.cpp' file must be located
+     * in this directory ('src/'). If you want to link against
+     * an external library (e.g. the gsl), you should add the
+     * corresponding compiler and linker flags to the "Makefile.conf"
+     * in the top level directory.
+     *
+     * Note: Not all functions listed here may actually be needed.
+     *       This file lists all derivatives that occurred in the
+     *       calculation. It is possible that some dropped out due
+     *       to algebraic simplifications after this list was
+     *       generated.
+     */
+
+/*
+%s
+
+%s
+
+%s
+
+*/
+
+};
+#endif
---
+++ high_level_tests/make_package_selective_ibp/generate_userdefined_cpp.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import shutil
+from itertools import permutations
+from pySecDec.code_writer import make_package
+
+if __name__ == "__main__":
+
+    name = 'userdefined_cpp'
+
+    make_package(
+
+    name=name,
+    integration_variables = ['x','y','z'],
+
+    # use integration by parts rather than subtraction for ``x`` and ``y``
+    # generate subtraction term for ``z`` to avoid derivative of HeavisideTheta
+    ibp_power_goal = [0,0,-1],
+
+    # the order here defines the order of the expansion
+    regulators = ['eps'],
+    functions = ['HeavisideTheta','func'],
+
+    polynomials_to_decompose = ['(x+y)**(-2+eps)','z**(-1+eps)'],
+    remainder_expression = 'HeavisideTheta(1/4-z)*func(y)',
+    # full analytic result for func(y)=1:
+    #   (4^-eps (-2 + 2^eps))/((-1 + eps) eps^2)
+    # = 1/eps^2 + (1 - Log[2] - Log[4])/eps +  1/2 (2 - 2 Log[2] - Log[2]^2 - 2 Log[4] + 2 Log[2] Log[4] + Log[4]^2) + O[eps]
+    # = 1.0000000000000000000/eps^2 - 1.0794415416798359283/eps + 0.60214400703386905808 + O[eps]
+
+    # the highest orders of the final regulator expansion
+    # the order here matches the order of ``regulators``
+    requested_orders = [0],
+
+    )
+
+    # check the generated "functions.hpp"
+    cpp_function_declarations = (
+    '    template<typename T0>\n    integrand_return_t HeavisideTheta(T0 arg0);',
+    '    template<typename T0>\n    integrand_return_t dfuncd0(T0 arg0);',
+    '    template<typename T0>\n    integrand_return_t func(T0 arg0);'
+    )
+    with open(name + '/src/functions.hpp') as generated_header_file:
+        generated_header = generated_header_file.read()
+    with open('functions_template.hpp') as target_header_template_file:
+        target_header_template = target_header_template_file.read()
+    matched_header = False
+    for ordering in permutations(cpp_function_declarations):
+        if generated_header == target_header_template % ordering:
+            matched_header = True
+            break
+    assert matched_header, 'mismatch between generated and expected "functions.hpp"'
+
+    # copy 'functions.hpp' (predefined for this example) to required directory
+    shutil.copy('functions_implementation.hpp',name+'/src/functions.hpp')
