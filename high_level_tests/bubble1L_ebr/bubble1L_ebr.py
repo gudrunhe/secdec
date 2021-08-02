bbb70ee0f9bc38d3c7b9a890682d3f15adfb1657
Stephen Jones <s.jones@cern.ch>
2021-08-02 13:50:50 +0100

    [high_level_tests] promote bubble1L_ebr to testcase (loop_regions)

---
+++ high_level_tests/bubble1L_ebr/bubble1L_ebr.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from pySecDec.code_writer import sum_package
+from pySecDec.loop_integral import loop_regions
+import pySecDec as psd
+
+if __name__ == "__main__":
+
+    # this object represents the Feynman graph
+    li = psd.loop_integral.LoopIntegralFromGraph(
+            internal_lines = [['m',[1,2]],['m',[2,1]]],
+            external_lines = [['p',1],['p',2]],
+            replacement_rules = [('p*p', 's'), ('m*m', 'z*msq')])
+
+    # find the regions and expand the integrals using expansion by regions
+    regions_generator_args = loop_regions(
+        name = 'bubble1L_ebr',
+        loop_integral = li,
+        smallness_parameter = 'z',
+        expansion_by_regions_order = 1)
+
+    # generate code that will calculate the sum of all regions and all orders in
+    # the smallness parameter
+    sum_package('bubble1L_ebr',
+        regions_generator_args,
+        regulators = ['eps'],
+        requested_orders = [0],
+        real_parameters = ['s','msq','z'],
+        complex_parameters = [])
