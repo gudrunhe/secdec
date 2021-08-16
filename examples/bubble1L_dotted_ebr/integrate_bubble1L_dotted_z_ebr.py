8db04118c75fc3c59279c1cf25ae252a1f7466fb
Stephen Jones <s.jones@cern.ch>
2021-08-16 21:31:45 +0100

    [examples] use series_to_sympy in bubble1L_dotted_ebr

--- examples/bubble1L_dotted_ebr/integrate_bubble1L_dotted_z_ebr.py
+++ examples/bubble1L_dotted_ebr/integrate_bubble1L_dotted_z_ebr.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from pySecDec.integral_interface import IntegralLibrary
+from pySecDec.integral_interface import IntegralLibrary, series_to_sympy
 import sympy as sp
 import numpy as np
 
@@ -23,29 +23,24 @@ if __name__ == "__main__":
     name = "bubble1L_dotted_z"
     real_parameters = [psq,msq,1] # z set to 1
 
-# load c++ library
+    # load c++ library
     intlib = IntegralLibrary(f"{name}/{name}_pylink.so".format(name))
     intlib.use_Qmc(transform="korobov3", fitfunction="polysingular")
 
-# integrate
+    # integrate
     str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = intlib(real_parameters)
 
-# convert complex numbers from c++ to sympy notation
-    str_integral_with_prefactor = str_integral_with_prefactor.replace(',','+I*')
+    # convert the result to sympy expressions
+    result, error = map(sp.sympify, series_to_sympy(str_integral_with_prefactor))
 
-# convert result to sympy expressions
-    integral_result = sp.sympify(str_integral_with_prefactor.replace('+/-','*value+error*'))
-    integral_result_err = sp.sympify(str_integral_with_prefactor.replace('+/-','*value+error*'))
-
-# examples how to access individual orders
-    print('Numerical Result')
+    # access and print individual terms of the expansion
+    print("Numerical Result")
     for power in [-2, -1, 0]:
-        valreal, valimg = integral_result.coeff('eps',power).coeff('value').as_real_imag()
-        errreal, errimg = integral_result.coeff('eps',power).coeff('error').as_real_imag()
-        print("eps^{:<2} {: .5f}{:+.5f}*I +/- {: .5e}{:+.5e}*I".format(power,float(valreal),float(valimg),float(errreal),float(errimg)))
+        val = complex(result.coeff("eps", power))
+        err = complex(error.coeff("eps", power))
+        print(f"eps^{power:<2} {val: .5f} +/- {err:.5e}")
 
     order = 1
     print("psq, msq: {}, {}".format(psq,msq))
     print("exact result: {:.5f}".format(f(psq,msq)))
     print("approximate result to order {}: {:.5f}".format(order,fapprox(psq,msq,order)))
-    print()
