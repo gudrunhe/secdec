841e35b559e91caf7b6ee352343d6f189dad09e8
Stephen Jones <s.jones@cern.ch>
2021-08-16 21:42:22 +0100

    [examples] use t-method for bubble2L_largem_ebr, use series_to_sympy

--- examples/bubble2L_largem_ebr/generate_bubble2L_largem_ebr.py
+++ examples/bubble2L_largem_ebr/generate_bubble2L_largem_ebr.py
@@ -11,19 +11,18 @@ if __name__ == "__main__":
     propagators = ['k1**2-msq_','(k1+p1)**2-msq_','(k1-k2)**2-msq_','k2**2','(k2+p1)**2'],
     loop_momenta = ['k1','k2'],
     external_momenta = ['p1'],
-    replacement_rules = [('p1*p1', 'psq'), ('msq_','msq/z')])
+    replacement_rules = [('p1*p1', 'psq'), ('msq_','msq')])
 
     # find the regions and expand the integrals using expansion by regions
     regions_generator_args = psd.loop_regions(
         name = 'bubble2L_largem_ebr',
         loop_integral = li,
-        smallness_parameter = 'z',
-        additional_prefactor = '-1',
-        expansion_by_regions_order = 1)
+        smallness_parameter = 'psq',
+        additional_prefactor = '-1')
 
     # generate code that will calculate the sum of all regions and all orders in
     # the smallness parameter
     psd.sum_package('bubble2L_largem_ebr',
         regions_generator_args, regulators = ['eps'],requested_orders = [0],
-        real_parameters = ['psq','msq','z'],
+        real_parameters = ['psq','msq'],
         complex_parameters = [])
