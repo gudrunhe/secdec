0f457b29acd7b508dba9f2d24ce15eed33a8c38d
Stephen Jones <s.jones@cern.ch>
2021-08-19 12:25:04 +0100

    [examples] use t-method for bubble2L_smallm_ebr, use series_to_sympy

--- examples/bubble2L_smallm_ebr/generate_bubble2L_smallm.py
+++ examples/bubble2L_smallm_ebr/generate_bubble2L_smallm.py
@@ -11,18 +11,18 @@ if __name__ == "__main__":
     propagators = ['k1**2-msq_','(k1+k2)**2-msq_','(k1+p1)**2','k2**2','(k1+k2+p1)**2'],
     loop_momenta = ['k1','k2'],
     external_momenta = ['p1'],
-    replacement_rules = [('p1*p1', 'psq'), ('msq_','z*msq')])
+    replacement_rules = [('p1*p1', 'psq'), ('msq_','msq')])
 
     # find the regions and expand the integrals using expansion by regions
     regions_generator_args = psd.loop_regions(
         name = 'bubble2L_smallm',
         loop_integral = li,
-        smallness_parameter = 'z',
-        expansion_by_regions_order = 2) # this has to be 2 to catch the ultra-soft region
+        smallness_parameter = 'msq',
+        expansion_by_regions_order = 1) # this has to be 1 to catch the ultra-soft region
 
     # generate code that will calculate the sum of all regions and all orders in
     # the smallness parameter
     psd.sum_package('bubble2L_smallm',
         regions_generator_args, regulators = ['eps'],requested_orders = [0],
-        real_parameters = ['psq','msq','z'],
+        real_parameters = ['psq','msq'],
         complex_parameters = [])
--- examples/bubble2L_smallm_ebr/integrate_bubble2L_smallm.py
+++ examples/bubble2L_smallm_ebr/integrate_bubble2L_smallm.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from pySecDec.integral_interface import IntegralLibrary
+from pySecDec.integral_interface import IntegralLibrary, series_to_sympy
 import sympy as sp
 
 if __name__ == "__main__":
@@ -11,18 +11,16 @@ if __name__ == "__main__":
     intlib.use_Qmc(transform="korobov3", fitfunction="polysingular", verbosity=1)
 
     # integrate
-    str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = intlib(real_parameters=[4,0.002,1])
+    str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = intlib(real_parameters=[4,0.002])
+    
+    print(str_integral_with_prefactor)
 
-    # convert complex numbers from c++ to sympy notation
-    str_integral_with_prefactor = str_integral_with_prefactor.replace(',','+I*')
-
-    # convert result to sympy expressions
-    integral_result = sp.sympify(str_integral_with_prefactor.replace('+/-','*value+error*'))
-    integral_result_err = sp.sympify(str_integral_with_prefactor.replace('+/-','*value+error*'))
+    # convert the result to sympy expressions
+    result, error = map(sp.sympify, series_to_sympy(str_integral_with_prefactor))
 
     # examples how to access individual orders
     print('Numerical Result')
     for power in [-1, 0]:
-        valreal, valimg = integral_result.coeff('eps',power).coeff('value').as_real_imag()
-        errreal, errimg = integral_result.coeff('eps',power).coeff('error').as_real_imag()
+        valreal, valimg = result.coeff('eps',power).as_real_imag()
+        errreal, errimg = error.coeff('eps',power).as_real_imag()
         print("eps^{:<2} {: .15f}{:+.15f}*I +/- {:.15f}{:+.15f}*I".format(power,float(valreal),float(valimg),float(errreal),float(errimg)))
