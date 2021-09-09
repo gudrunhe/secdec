8a2d322725f87b6626b0be1d17f19b79e8c6a9cf
Gdrun Heinrich <gudrun.heinrich@kit.edu>
2021-09-09 16:35:04 +0200

    [nodist_examples] 4loop massive banana added; write_integrand.frm exits with Overflow in Multiplication

---
+++ nodist_examples/banana4L/integrate_banana4L.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from pySecDec.integral_interface import IntegralLibrary
+
+if __name__ == "__main__":
+
+    # load c++ library
+    banana_4L_3mass = IntegralLibrary('banana_4L_3mass/banana_4L_3mass_pylink.so')
+
+    # choose integrator
+    banana_4L_3mass.use_Qmc(verbosity=0,minn=10**6,maxeval=1,transform='korobov2')
+
+    # integrator settings used to run the timings
+    #banana_4L_3mass.use_Qmc(verbosity=2,minn=10**5,maxeval=10**5,transform='korobov2')
+    #banana_4L_3mass.use_Qmc(verbosity=2,minn=10**5,maxeval=10**5,transform='korobov2',devices=[-1])
+    #banana_4L_3mass.use_Vegas(flags=2,maxeval=10**6,epsrel=1e-100,epsabs=1e-100)
+
+    # integrate
+    str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = banana_4L_3mass(real_parameters=[30.0,1.0,1.3,0.7], deformation_parameters_maximum=0.1, verbose=True)
+
+    # print results
+    print('Numerical Result')
+    print('integral without prefactor', str_integral_without_prefactor)
+    print('prefactor', str_prefactor)
+    print('integral with prefactor', str_integral_with_prefactor)
+
