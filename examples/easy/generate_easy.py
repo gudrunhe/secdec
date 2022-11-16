39b685c138eecb53fc43acc10a2009939521ad59
Vitaly Magerya <magv@tx97.net>
2022-11-16 16:29:22 +0100

    [examples] merge easy and easy_disteval

--- examples/easy/generate_easy.py
+++ examples/easy/generate_easy.py
@@ -5,11 +5,10 @@ from pySecDec import make_package
 if __name__ == "__main__":
 
     make_package(
+        name = 'easy',
+        integration_variables = ['x','y'],
+        regulators = ['eps'],
 
-    name = 'easy',
-    integration_variables = ['x','y'],
-    regulators = ['eps'],
-
-    requested_orders = [0],
-    polynomials_to_decompose = ['(x+y)^(-2+eps)'],
+        requested_orders = [0],
+        polynomials_to_decompose = ['(x+y)^(-2+eps)']
     )
