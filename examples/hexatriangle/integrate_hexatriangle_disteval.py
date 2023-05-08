8975f43b1e14b209c53c32259ab1649245895645
Vitaly Magerya <magv@tx97.net>
2023-05-08 15:29:25 +0200

    [examples] print the known values in the hexatriangle example

--- examples/hexatriangle/integrate_hexatriangle_disteval.py
+++ examples/hexatriangle/integrate_hexatriangle_disteval.py
@@ -7,4 +7,5 @@ lib = DistevalLibrary('hexatriangle/disteval/hexatriangle.json')
 p = {"mh2": 12/23, 'mt2': 1, 'x12': 262/35, 'x23': -145/53, 'x35': 327/164, 'x41': -101/36, 'x54': 249/124}
 result = lib(p, points=10000, number_of_presamples=10000, epsrel=1e-3, epsabs=1e-10, verbose=True)
 
-print(result)
+print("Known value: 1.454919812(7)*10^-7 - 1.069797219(8)*10^-07 I + O(eps)")
+print("Calculated value:", result)
--- examples/hexatriangle/integrate_hexatriangle_intlib.py
+++ examples/hexatriangle/integrate_hexatriangle_intlib.py
@@ -9,4 +9,5 @@ lib.use_Qmc(verbosity=0, minn=10000)
 p = [12/23, 1, 262/35, -145/53, 327/164, -101/36, 249/124]
 str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = lib(p, epsrel=1e-3, epsabs=1e-10, verbose=True, number_of_presamples=10000)
 
-print(str_integral_with_prefactor)
+print("Known value: 1.454919812(7)*10^-7 - 1.069797219(8)*10^-07 I + O(eps)")
+print("Calculated value:", str_integral_with_prefactor)
