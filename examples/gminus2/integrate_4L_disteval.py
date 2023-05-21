55260636bcd694c03d19236fe1b9c38010d8a11d
Anton Olsson <anton.olsson@kit.edu>
2023-05-21 22:55:14 +0200

    [examples] print ALL orders of the g-2 4L result

--- examples/gminus2/integrate_4L_disteval.py
+++ examples/gminus2/integrate_4L_disteval.py
@@ -17,6 +17,8 @@ if __name__ == "__main__":
 
     # examples how to access individual orders
     print('Numerical Result')
+    print('eps^-4:', value.coeff('eps',-4), '+/- (', error.coeff('eps',-4), ')')
+    print('eps^-3:', value.coeff('eps',-3), '+/- (', error.coeff('eps',-3), ')')
     print('eps^-2:', value.coeff('eps',-2), '+/- (', error.coeff('eps',-2), ')')
     print('eps^-1:', value.coeff('eps',-1), '+/- (', error.coeff('eps',-1), ')')
     print('eps^0 :', value.coeff('eps',0), '+/- (', error.coeff('eps',0), ')')
