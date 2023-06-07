a00d2846a8779bee55c764513475c9e8cedc2dbb
Stephen Jones <s.jones@cern.ch>
2023-06-07 16:10:28 +0100

    [high_level_tests] add check for disteval, disteval-cli

--- high_level_tests/easy_sum/test/test.py
+++ high_level_tests/easy_sum/test/test.py
@@ -1,12 +1,16 @@
 from __future__ import print_function
-from pySecDec.integral_interface import IntegralLibrary
+from pySecDec.integral_interface import IntegralLibrary, DistevalLibrary
 import sympy as sp
+import sys
+import subprocess
+import json
 import unittest
 
 class CheckLib(unittest.TestCase):
     def setUp(self):
-        # load c++ library
+        # load c++ libraries
         self.lib = IntegralLibrary('../easy_sum/easy_sum_pylink.so')
+        self.distlib = DistevalLibrary('../easy_sum/disteval/easy_sum.json')
 
         # set global options
         self.real_parameters = [0.1]
@@ -32,13 +36,26 @@ class CheckLib(unittest.TestCase):
         self.order_min_2 =  -2
         self.order_max_2 =  0
 
-    def check_result(self, computed_series, target_series, epsrel, epsabs, order_min ,order_max):
-        # convert result to sympy expressions
-        computed_series = sp.sympify(  computed_series.replace(',','+I*').replace('+/-','*value+error*')  )
+    def check_result(self, computed_series, target_series, epsrel, epsabs, order_min ,order_max, integrator='intlib'):
+    
+        if integrator == 'intlib':
+            # convert result to sympy expressions
+            computed_series = sp.sympify(  computed_series.replace(',','+I*').replace('+/-','*value+error*')  )
 
-        for order in range(order_min, order_max+1):
-            value = complex( computed_series.coeff('eps',order).coeff('value') )
-            error = complex( computed_series.coeff('eps',order).coeff('error') )
+        for i, order in enumerate(range(order_min, order_max+1)):
+        
+            if integrator == 'intlib':
+                value = complex( computed_series.coeff('eps',order).coeff('value') )
+                error = complex( computed_series.coeff('eps',order).coeff('error') )
+            elif integrator == 'disteval':
+                value = computed_series[(order,)][0]
+                error = computed_series[(order,)][1]
+            elif integrator == 'disteval-cli':
+                self.assertEqual(computed_series[i][0][0],order)
+                value = complex(computed_series[i][1][0],computed_series[i][1][1])
+                error = complex(computed_series[i][2][0],computed_series[i][2][1])
+            else:
+                raise ValueError(f"unknown integrator in check_result, got {integrator}")
 
             # check that the uncertainties are reasonable
             if target_series[order].real != 0.0:
@@ -78,7 +95,22 @@ class CheckLib(unittest.TestCase):
         # check integral
         self.check_result(str_integral_with_prefactor_1, self.target_result_with_prefactor_1, self.epsrel, self.epsabs, self.order_min_1, self.order_max_1)
         self.check_result(str_integral_with_prefactor_2, self.target_result_with_prefactor_2, self.epsrel, self.epsabs, self.order_min_2, self.order_max_2)
+        
+    def test_disteval(self):
+        # integrate
+        result = self.distlib(parameters={'s':self.real_parameters[0]}, epsrel=self.epsrel, epsabs=self.epsabs, format='json', verbose=False)
 
+        # check integral
+        self.check_result(result['sums']['sum0'], self.target_result_with_prefactor_1, self.epsrel, self.epsabs, self.order_min_1, self.order_max_1, integrator='disteval')
+        self.check_result(result['sums']['sum1'], self.target_result_with_prefactor_2, self.epsrel, self.epsabs, self.order_min_2, self.order_max_2, integrator='disteval')
+        
+    def test_disteval_cli(self):
+        psd_out = subprocess.run([f'{sys.executable} -m pySecDec.disteval ../easy_sum/disteval/easy_sum.json --epsabs={self.epsabs} --epsrel={self.epsrel} s={self.real_parameters[0]} --format="json"'], shell=True, check=True, capture_output=True)
+        result = json.loads(psd_out.stdout)
+        
+        # check integral
+        self.check_result(result['sums']['sum0'], self.target_result_with_prefactor_1, self.epsrel, self.epsabs, self.order_min_1, self.order_max_1, integrator='disteval-cli')
+        self.check_result(result['sums']['sum1'], self.target_result_with_prefactor_2, self.epsrel, self.epsabs, self.order_min_2, self.order_max_2, integrator='disteval-cli')
 
 if __name__ == '__main__':
     unittest.main()
