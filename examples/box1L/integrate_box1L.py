ca7717b379863d35f0bb76b174e74535bb142e6e
Stephen Jones <s.jones@cern.ch>
2023-08-15 13:51:18 +0100

    [examples] tidy gettingstarted examples

--- examples/box1L/integrate_box1L.py
+++ examples/box1L/integrate_box1L.py
@@ -8,28 +8,18 @@ if __name__ == "__main__":
     box1L = IntegralLibrary('box1L/box1L_pylink.so')
 
     # choose integrator
-    box1L.use_Qmc(epsrel=1e-3, epsabs=1e-10)
+    box1L.use_Qmc()
 
     # integrate
-    str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = box1L(real_parameters=[4.0, -0.75, 1.25, 1.0], verbose=True)
-
-    # convert complex numbers from c++ to sympy notation
-    str_integral_with_prefactor = str_integral_with_prefactor.replace(',','+I*')
-    str_prefactor = str_prefactor.replace(',','+I*')
-    str_integral_without_prefactor = str_integral_without_prefactor.replace(',','+I*')
-
-    # convert result to sympy expressions
-    integral_with_prefactor = sp.sympify(str_integral_with_prefactor.replace('+/-','*value+error*'))
-    integral_with_prefactor_err = sp.sympify(str_integral_with_prefactor.replace('+/-','*value+error*'))
-    prefactor = sp.sympify(str_prefactor)
-    integral_without_prefactor = sp.sympify(str_integral_without_prefactor.replace('+/-','*value+error*'))
-    integral_without_prefactor_err = sp.sympify(str_integral_without_prefactor.replace('+/-','*value+error*'))
+    result_without_prefactor, result_prefactor, result_with_prefactor = box1L(real_parameters=[4.0, -0.75, 1.25, 1.0], 
+                                                                              epsrel=1e-3, epsabs=1e-10, format="json")
+    values = result_with_prefactor["sums"]["box1L"]
 
     # examples how to access individual orders
     print('Numerical Result')
-    print('eps^-2:', integral_with_prefactor.coeff('eps',-2).coeff('value'), '+/- (', integral_with_prefactor_err.coeff('eps',-2).coeff('error'), ')')
-    print('eps^-1:', integral_with_prefactor.coeff('eps',-1).coeff('value'), '+/- (', integral_with_prefactor_err.coeff('eps',-1).coeff('error'), ')')
-    print('eps^0 :', integral_with_prefactor.coeff('eps',0).coeff('value'), '+/- (', integral_with_prefactor_err.coeff('eps',0).coeff('error'), ')')
+    print('eps^-2:', values[(-2,)][0], '+/- (', values[(-2,)][1], ')')
+    print('eps^-1:', values[(-1,)][0], '+/- (', values[(-1,)][1], ')')
+    print('eps^0 :', values[( 0,)][0], '+/- (', values[( 0,)][1], ')')
 
     print('Analytic Result')
     print('eps^-2: -0.1428571429')
--- examples/easy/generate_easy.py
+++ examples/easy/generate_easy.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
         name = 'easy',
         integration_variables = ['x','y'],
         regulators = ['eps'],
-
         requested_orders = [0],
         polynomials_to_decompose = ['(x+y)^(-2+eps)']
     )
